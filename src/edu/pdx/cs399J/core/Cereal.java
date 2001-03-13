0732c37d23b814677247247ebbf792afd4f3b9d0
David Whitlock <david.m.whitlock@gmail.com>
2001-03-13 23:33:38 +0000

    Added Ceral.java Comparator example

---
+++ src/edu/pdx/cs399J/core/Cereal.java
@@ -0,0 +1,35 @@
+package edu.pdx.cs410J.examples;
+
+/**
+ * This class represents a box of cereal.  It has a name and a price.
+ */
+public class Cereal {
+  private String name;
+  private double price;
+
+  /**
+   * Creates a new box of cereal
+   */
+  public Cereal(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  /**
+   * Returns the name of this cereal
+   */
+  public String getName() {
+    return(this.name);
+  }
+
+  /**
+   * Returns the price of this cereal
+   */
+  public double getPrice() {
+    return(this.price);
+  }
+
+  public String toString() {
+    return(this.name + " $" + this.price);
+  }
+}
---
+++ src/edu/pdx/cs410J/core/CerealComparator.java
@@ -0,0 +1,36 @@
+package edu.pdx.cs410J.examples;
+
+import java.util.*;
+
+/**
+ * Compares two boxes of ceral by their price
+ */
+public class CerealComparator implements Comparator {
+  public int compare(Object o1, Object o2) {
+    double price1 = ((Cereal) o1).getPrice();
+    double price2 = ((Cereal) o2).getPrice();
+
+    if(price1 > price2) {
+      return(1);
+    } else if(price1 < price2) {
+      return(-1);
+    } else {
+      return(0);
+    }
+  }
+
+  public static void main(String[] args) {
+    Set set = new TreeSet(new CerealComparator());
+    set.add(new Cereal("Cap'n Crunch", 2.59));
+    set.add(new Cereal("Trix", 3.29));
+    set.add(new Cereal("Count Chocula", 2.59));
+    set.add(new Cereal("Froot Loops", 2.45));
+
+    // Print out the cereals
+    Iterator iter = set.iterator();
+    while(iter.hasNext()) {
+      Cereal c = (Cereal) iter.next();
+      System.out.println(c);
+    }
+  }
+}
