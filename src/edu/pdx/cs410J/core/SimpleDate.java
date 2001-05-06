bd59031e37c509a827ba75693f88626f73b064b8
David Whitlock <david.m.whitlock@gmail.com>
2001-05-06 04:59:10 +0000

    Added example of SimpleDateFormat

---
+++ src/edu/pdx/cs410J/core/SimpleDate.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.examples;
+
+import java.text.*;
+import java.util.*;
+
+/**
+ * This program demonstrates how to use the {@link
+ * java.text.SimpleDateFormat} class to format and parse dates.
+ */
+public class SimpleDate {
+
+  public static void main(String[] args) {
+    DateFormat df = new SimpleDateFormat(args[0]);
+    Date now = new Date();
+    System.out.println(df.format(now));
+  }
+
+}
