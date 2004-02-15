906e5c163ca6d5c6d52ae576b94736178acf2aca
David Whitlock <david.m.whitlock@gmail.com>
2004-02-15 22:46:11 +0000

    dump() method now throws IOException

--- src/edu/pdx/cs399J/AppointmentBookDumper.java
+++ src/edu/pdx/cs399J/AppointmentBookDumper.java
@@ -1,10 +1,14 @@
 package edu.pdx.cs399J;
 
+import java.io.IOException;
+
 /**
  * This interface allows the contents of an appointment book to be
  * dumped to some destination.
  *
  * @author David Whitlock
+ * @version $Revision: 1.2 $
+ * @since Fall 2000
  */
 public interface AppointmentBookDumper {
 
@@ -13,7 +17,10 @@ public interface AppointmentBookDumper {
    *
    * @param book
    *        The appointment book whose contents are to be dumped
+   *
+   * @throws IOException
+   *         Something went wrong while dumping the appointment book
    */
-  public void dump(AbstractAppointmentBook book);
+  public void dump(AbstractAppointmentBook book) throws IOException;
 
 }
--- src/edu/pdx/cs399J/PhoneBillDumper.java
+++ src/edu/pdx/cs399J/PhoneBillDumper.java
@@ -3,6 +3,10 @@ package edu.pdx.cs399J;
 /**
  * Classes that implement this interface dump the contents of a phone
  * bill to some destination.
+ *
+ * @author David Whitlock
+ * @version $Revision: 1.2 $
+ * @since Spring 2001
  */
 public interface PhoneBillDumper {
 
