43fccb3b916aa74b140b52045ae190f012539489
David Whitlock <david.m.whitlock@gmail.com>
2005-11-25 23:38:13 +0000

    Added ColorList

---
+++ src/edu/pdx/cs410G/mvc/ColorList.java
@@ -0,0 +1,25 @@
+package edu.pdx.cs410G.mvc;
+
+import javax.swing.*;
+
+/**
+ * This class demonstrates a {@link ListCellRenderer} that displays
+ * the elements of a list in a particular color.
+ *
+ * @author David Whitlock
+ * @since Fall 2005
+ */
+public class ColorList extends JPanel {
+
+  /**
+   * Populates a <code>ColorList</code> with a single {@link JList}
+   * that contains {@link ColorList.NamedColor}s and is drawn with a
+   * custom {@link ListCellRenderer}.
+   */
+  public ColorList() {
+
+
+
+  }
+
+}
