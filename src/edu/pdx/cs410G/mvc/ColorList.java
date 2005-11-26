0a9e565b355ba5631f1f32fbfd1997ab087bf8f3
David Whitlock <david.m.whitlock@gmail.com>
2005-11-26 00:17:09 +0000

    Implemented ColorList

--- src/edu/pdx/cs410G/mvc/ColorList.java
+++ src/edu/pdx/cs410G/mvc/ColorList.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410G.mvc;
 
+import java.awt.Color;
+import java.awt.Component;
 import javax.swing.*;
 
 /**
@@ -17,9 +19,55 @@ public class ColorList extends JPanel {
    * custom {@link ListCellRenderer}.
    */
   public ColorList() {
+    DefaultListModel model = new DefaultListModel();
+    model.addElement(new NamedColor(Color.RED, "Red"));
+    model.addElement(new NamedColor(Color.GREEN, "Green"));
+    model.addElement(new NamedColor(Color.BLUE, "Blue"));
+    model.addElement(new NamedColor(Color.BLACK, "Black"));
+    model.addElement(new NamedColor(Color.ORANGE, "Orange"));
 
+    ListCellRenderer renderer = new DefaultListCellRenderer() {
+        public Component getListCellRendererComponent(JList list,
+                                                      Object value,
+                                                      int index,
+                                                      boolean isSelected,
+                                                      boolean cellHasFocus) {
+          JLabel label = (JLabel)
+            super.getListCellRendererComponent(list, value, index, 
+                                               isSelected, cellHasFocus);
+          NamedColor nc = (NamedColor) value;
+          label.setForeground(nc.color);
+          label.setText(nc.name);
 
+          return label;
+        }
+      };
 
+    JList list = new JList(model);
+    list.setCellRenderer(renderer);
+
+    this.add(list);    
+  }
+
+  /**
+   * A class that consists of a {@link Color} and its name
+   */
+  static class NamedColor {
+    final Color color;
+    final String name;
+
+    NamedColor(Color color, String name) {
+      this.color = color;
+      this.name = name;
+    }
+  }
+
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Using a ListCellRenderer");
+    frame.getContentPane().add(new ColorList());
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
   }
 
 }
