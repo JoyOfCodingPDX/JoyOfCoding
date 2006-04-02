f97af253e2e08864f0399aadfaee611552ad0b57
David Whitlock <david.m.whitlock@gmail.com>
2006-04-02 17:08:57 +0000

    Added example Ant task and updated the LRU Map to use generics

--- src/edu/pdx/cs399J/AbstractLRUMap.java
+++ src/edu/pdx/cs399J/AbstractLRUMap.java
@@ -10,7 +10,7 @@ import java.util.*;
  * of mappings, then the least recently used mapping is removed.  An
  * element is used when it is added to or gotten from the Map.
  */
-public abstract class AbstractLRUMap extends AbstractMap {
+public abstract class AbstractLRUMap<K, V> extends AbstractMap<K, V> {
 
   /** The maximum number of mappings that this map can hold */
   protected int capacity;
@@ -110,31 +110,38 @@ public abstract class AbstractLRUMap extends AbstractMap {
   //////////////////////  Instance Methods  /////////////////////
 
   /**
-   * Returns the names (as {@link String}s) of the students who
-   * implemented this LRU Map.
+   * Returns the names of the students who implemented this LRU Map.
    */
-  public abstract List getStudentNames();
+  public abstract List<String> getStudentNames();
 
   /**
    * When a mapping is made in an LRU that already contains the
    * maximum number of mappings, the Least Recently Used element is
    * removed.
    */
-  public abstract Object put(Object key, Object value);
+  public abstract V put(K key, V value);
 
   /**
    * Getting an element from a map marks the mapping as "used"
    */
-  public abstract Object get(Object key);
+  public abstract V get(Object key);
 
-  public abstract Object remove(Object key);
+  public abstract V remove(Object key);
 
-  public void putAll(Map map) {
-    Iterator iter = map.entrySet().iterator();
+  public void putAll(Map<? extends K, ? extends V> map) {
+    Iterator<? extends K> iter = map.keySet().iterator();
     while (iter.hasNext()) {
-      Map.Entry entry = (Map.Entry) iter.next();
-      this.put(entry.getKey(), entry.getValue());
+      K key = iter.next();
+      V value = map.get(key);
+      this.put(key, value);
     }
+
+    // I couldn't figure out how to use the entrySet().  Nasty.
+//    Iterator<Map.Entry<? extends K, ? extends V>> iter = map.entrySet().iterator();
+//     while (iter.hasNext()) {
+//       Map.Entry<? extends K, ? extends V> entry = iter.next();
+//       this.put(entry.getKey(), entry.getValue());
+//     }
   }
 
   public abstract void clear();
