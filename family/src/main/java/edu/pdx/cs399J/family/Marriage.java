c1f2988188d9f7ce3f10411ae2b24bfbba57a1ea
David Whitlock <david.m.whitlock@gmail.com>
2008-03-08 23:09:21 +0000

    Move family tree code to a Maven project

---
+++ family/src/main/java/edu/pdx/cs399J/family/Marriage.java
@@ -0,0 +1,140 @@
+package edu.pdx.cs399J.family;
+
+import java.text.*;
+import java.util.*;
+
+/**
+ * This class represents a marriage between two people.  Ain't love
+ * grand?  Each marriage consists of a husband, a wife, a date, and a
+ * location.
+ *
+ * @author David Whitlock
+ */
+public class Marriage {
+
+  private Person husband;
+  private Person wife;
+  private Date date;
+  private String location;
+
+  /**
+   * Creates a marriage between a husband and a wife.  It is the
+   * responsibility of the caller to invoke {@link
+   * Person#addMarriage(Marriage)}.
+   *
+   * @throws IllegalArgumentException
+   *         The <code>husband</code> is not {@link Person#MALE} or
+   *         the <code>wife</code> is not {@link Person#FEMALE}.
+   */
+  public Marriage(Person husband, Person wife) {
+    if (husband.getGender() != Person.MALE) {
+      String s = "The husband in a Marriage must be MALE";
+      throw new IllegalArgumentException(s);
+    }
+
+    if (wife.getGender() != Person.FEMALE) {
+      String s = "The wife in a Marriage must be FEMALE";
+      throw new IllegalArgumentException(s);
+    }
+
+    this.husband = husband;
+    this.wife = wife;
+  }
+
+  /**
+   * Returns the husband in this marriage.
+   */
+  public Person getHusband() {
+    return this.husband;
+  }
+
+  /**
+   * Returns the wife in this marriage.
+   */
+  public Person getWife() {
+    return this.wife;
+  }
+
+  /**
+   * Returns the date on which the husband and wife were married.
+   */
+  public Date getDate() {
+    return this.date;
+  }
+
+  /**
+   * Sets the date on which the husband and wife were married.
+   */
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  /**
+   * Returns the location at which the husband and wife were married.
+   */
+  public String getLocation() {
+    if (this.location == null) {
+      return null;
+
+    } else {
+      return this.location.trim();
+    }
+  }
+
+  /**
+   * Sets the location at which the husband and wife were married.
+   */
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  /**
+   * Returns a brief description of this marriage.
+   */
+  public String toString() {
+    StringBuffer sb = new StringBuffer();
+    sb.append(this.husband.getFullName() + " and " +
+	      this.wife.getFullName() + " were married");
+
+    if (this.date != null) {
+      DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+      sb.append(" on " + df.format(this.date));
+    }
+    if (this.location != null) {
+      sb.append(" in " + this.location);
+    }
+
+    return sb.toString();
+  }
+
+  /**
+   * A simple test program.
+   */
+  public static void main(String[] args) {
+    // Create my parent's marriage
+    Person me = Person.me();
+    Person mom = Person.mom(me);
+    Person dad = Person.dad(me);
+
+    Marriage marriage = new Marriage(dad, mom);
+    marriage.setLocation("Durham, NH");
+
+    try {
+      DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+      marriage.setDate(df.parse("Jul 12, 1969"));
+
+    } catch (ParseException ex) {
+      System.err.println("** Malformatted wedding day?");
+      System.exit(1);
+    }
+
+    mom.addMarriage(marriage);
+    dad.addMarriage(marriage);
+
+    // Print out some info
+    System.out.println(mom + "\n");
+    System.out.println(dad + "\n");
+    System.out.println(marriage + "\n");
+  }
+
+}
---
+++ family/src/main/java/edu/pdx/cs399J/family/tests/XmlTest.java
@@ -0,0 +1,63 @@
+package edu.pdx.cs399J.family.tests;
+
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import edu.pdx.cs399J.family.FamilyTree;
+import edu.pdx.cs399J.family.FamilyTreeException;
+import edu.pdx.cs399J.family.XmlDumper;
+import edu.pdx.cs399J.family.XmlParser;
+
+/**
+ * This class tests the functionality of the <code>XmlDumper</code>
+ * and <code>XmlParser</code> classes.
+ */
+public class XmlTest extends FamilyTreeConversionTestCase {
+
+  /**
+   * Returns a suite containing all of the tests in this class
+   */
+  public static Test suite() {
+    return(new TestSuite(XmlTest.class));
+  }
+
+  /**
+   * Creates a new <code>XmlTest</code> for running the test of a
+   * given name
+   */
+  public XmlTest(String name) {
+    super(name);
+  }
+
+  ////////  Helper Methods
+
+  /**
+   * Converts a FamilyTree to XML and returns the XML as a String.
+   */
+  protected String getStringFor(FamilyTree tree) {
+    // Write the XML to a String
+    StringWriter sw = new StringWriter();
+    PrintWriter pw = new PrintWriter(sw, true);
+    XmlDumper dumper = new XmlDumper(pw);
+    dumper.dump(tree);
+    String xml = sw.toString();
+    return xml;
+  }
+
+  /**
+   * Parsers a FamilyTree from a String containing XML
+   */
+  protected FamilyTree getFamilyTreeFor(String xml) 
+    throws FamilyTreeException {
+
+    // Parse the XML from the String
+    StringReader sr = new StringReader(xml);
+    XmlParser parser = new XmlParser(sr);
+    return parser.parse();
+  }
+
+
+}
