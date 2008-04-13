53a476c8f507e424d11932baebecf504a442a91f
David Whitlock <david.m.whitlock@gmail.com>
2008-04-13 05:05:56 +0000

    Added a couple more web examples.

--- web/src/main/java/edu/pdx/cs399J/servlets/ServletInfoServlet.java
+++ web/src/main/java/edu/pdx/cs399J/servlets/ServletInfoServlet.java
@@ -13,169 +13,180 @@ import javax.servlet.http.*;
  * @since Summer 2005
  */
 public class ServletInfoServlet extends HttpServlet {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
 
-	/**
-	 * Display information from the {@link HttpServletRequest}, {@link
-	 * HttpServletResponse}
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
+    doGet(request, response);
+  }
 
-		response.setContentType("text/html");
-		response.setBufferSize(8192);
-		PrintWriter out = response.getWriter();
+  protected void doPut(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
 
-		out.println("<html>");
-		out.println("<title>Servlet Information</title>");
-		out.println("<body>");
+    doGet(request, response);
+  }
 
-		out.println("<h1>Servlet Information " + (new Date()) + "</h1>");
+  /**
+   * Display information from the {@link HttpServletRequest}, {@link
+   * HttpServletResponse}
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
 
-		dump(this.getServletConfig().getServletContext(), out);
+    response.setContentType("text/html");
+    response.setBufferSize(8192);
+    PrintWriter out = response.getWriter();
 
-		dump(request, out);
+    out.println("<html>");
+    out.println("<title>Servlet Information</title>");
+    out.println("<body>");
 
-		out.println("</body>");
-		out.println("</html>");
-	}
+    out.println("<h1>Servlet Information " + (new Date()) + "</h1>");
 
-	/**
-	 * Starts a new HTML table with the given title
-	 */
-	private static void startTable(String title, PrintWriter out) {
-		out.println("<table border=\"1\">");
-		out.print("<caption align=\"top\">");
-		out.print(title);
-		out.println("</caption>");
-	}
+    dump(this.getServletConfig().getServletContext(), out);
 
-	/**
-	 * Inserts a row into an HTML table
-	 */
-	private static void tableRow(String key, Object value, PrintWriter out) {
-		out.print("  <tr><td>");
-		out.print(key);
-		out.print("</td><td>");
-		out.print(value);
-		out.println("</td></tr>");
-	}
+    dump(request, out);
 
-	/**
-	 * Ends an HTML table
-	 */
-	private static void endTable(PrintWriter out) {
-		out.println("</table>");
-	}
+    out.println("</body>");
+    out.println("</html>");
+  }
 
-	/**
-	 * Dumps information about a <code>HttpServletRequest</code> to the
-	 * given <code>PrintWriter</code>.
-	 */
-	private static void dump(HttpServletRequest request, PrintWriter out) {
-		startTable("HttpServletRequest", out);
+  /**
+   * Starts a new HTML table with the given title
+   */
+  private static void startTable(String title, PrintWriter out) {
+    out.println("<table border=\"1\">");
+    out.print("<caption align=\"top\">");
+    out.print(title);
+    out.println("</caption>");
+  }
 
-		for (Enumeration attrs = request.getAttributeNames(); attrs.hasMoreElements(); ) {
-			String name = (String) attrs.nextElement();
-			Object attr = request.getAttribute(name);
-			tableRow("Attribute \"" + name + "\"", attr, out);
-		}
+  /**
+   * Inserts a row into an HTML table
+   */
+  private static void tableRow(String key, Object value, PrintWriter out) {
+    out.print("  <tr><td>");
+    out.print(key);
+    out.print("</td><td>");
+    out.print(value);
+    out.println("</td></tr>");
+  }
 
-		tableRow("Character encoding", request.getCharacterEncoding(), out);
-		tableRow("Context length", String.valueOf(request.getContentLength()), out);
-		tableRow("Content type", request.getContentType(), out);
-		tableRow("Local address", request.getLocalAddr(), out);
-		tableRow("Local port", String.valueOf(request.getLocalPort()), out);
+  /**
+   * Ends an HTML table
+   */
+  private static void endTable(PrintWriter out) {
+    out.println("</table>");
+  }
 
-		tableRow("Remote address", request.getRemoteAddr(), out);
-		tableRow("Remote host", request.getRemoteHost(), out);
-		tableRow("Remote port", String.valueOf(request.getRemotePort()), out);
+  /**
+   * Dumps information about a <code>HttpServletRequest</code> to the
+   * given <code>PrintWriter</code>.
+   */
+  private static void dump(HttpServletRequest request, PrintWriter out) {
+    startTable("HttpServletRequest", out);
 
-		tableRow("Server name", request.getServerName(), out);
-		tableRow("Server port", String.valueOf(request.getServerPort()), out);
+    for (Enumeration attrs = request.getAttributeNames(); attrs.hasMoreElements();) {
+      String name = (String) attrs.nextElement();
+      Object attr = request.getAttribute(name);
+      tableRow("Attribute \"" + name + "\"", attr, out);
+    }
 
-		tableRow("Locale", request.getLocale(), out);
+    tableRow("Character encoding", request.getCharacterEncoding(), out);
+    tableRow("Context length", String.valueOf(request.getContentLength()), out);
+    tableRow("Content type", request.getContentType(), out);
+    tableRow("Local address", request.getLocalAddr(), out);
+    tableRow("Local port", String.valueOf(request.getLocalPort()), out);
 
-		{
-			StringBuffer sb = new StringBuffer();
-			for (Enumeration locales = request.getLocales(); locales.hasMoreElements(); ) {
-				sb.append(locales.nextElement());
-				sb.append(" ");
-			}
-			tableRow("Locales", sb.toString().trim(), out);
-		}
+    tableRow("Remote address", request.getRemoteAddr(), out);
+    tableRow("Remote host", request.getRemoteHost(), out);
+    tableRow("Remote port", String.valueOf(request.getRemotePort()), out);
 
-		for (Iterator iter = request.getParameterMap().entrySet().iterator(); iter.hasNext(); ) {
-			Map.Entry entry = (Map.Entry) iter.next();
-			Object param = entry.getValue();
-			if (param instanceof String[]) {
-				String[] array = (String[]) param;
-				StringBuffer sb = new StringBuffer();
-				for (int i = 0; i < array.length; i++) {
-					sb.append(array[i]);
-					if (i < array.length - 1) {
-						sb.append(", ");
-					}
-				}
-				param = sb;
-			}
-			tableRow("Parameter \"" + entry.getKey() + "\"", String.valueOf(param), out);
-		}
+    tableRow("Server name", request.getServerName(), out);
+    tableRow("Server port", String.valueOf(request.getServerPort()), out);
 
-		tableRow("Protocol", request.getProtocol(), out);
-		tableRow("Scheme", request.getScheme(), out);
-		tableRow("Is secure?", String.valueOf(request.isSecure()), out);
+    tableRow("Locale", request.getLocale(), out);
 
-		// HTTP
+    {
+      StringBuffer sb = new StringBuffer();
+      for (Enumeration locales = request.getLocales(); locales.hasMoreElements();) {
+        sb.append(locales.nextElement());
+        sb.append(" ");
+      }
+      tableRow("Locales", sb.toString().trim(), out);
+    }
 
-		tableRow("Method", request.getMethod(), out);
-		tableRow("Auth Type", request.getAuthType(), out);
+    for (Iterator iter = request.getParameterMap().entrySet().iterator(); iter.hasNext();) {
+      Map.Entry entry = (Map.Entry) iter.next();
+      Object param = entry.getValue();
+      if (param instanceof String[]) {
+        String[] array = (String[]) param;
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < array.length; i++) {
+          sb.append(array[i]);
+          if (i < array.length - 1) {
+            sb.append(", ");
+          }
+        }
+        param = sb;
+      }
+      tableRow("<b>Parameter</b> \"" + entry.getKey() + "\"", String.valueOf(param), out);
+    }
 
-		for (Enumeration names = request.getHeaderNames(); names.hasMoreElements(); ) {
-			String name = (String) names.nextElement();
-			String value = request.getHeader(name);
-			tableRow("Header \"" + name + "\"", value, out);
-		}
+    tableRow("Protocol", request.getProtocol(), out);
+    tableRow("Scheme", request.getScheme(), out);
+    tableRow("Is secure?", String.valueOf(request.isSecure()), out);
 
-		tableRow("Path Info", request.getPathInfo(), out);
-		tableRow("Path Translated", request.getPathTranslated(), out);
-		tableRow("Query String", request.getQueryString(), out);
-		tableRow("Remote User", request.getRemoteUser(), out);
-		tableRow("Requested Session ID", request.getRequestedSessionId(), out);
-		tableRow("Request URI", request.getRequestURI(), out);
-		tableRow("Request URL", request.getRequestURL(), out);
-		tableRow("Servlet Path", request.getServletPath(), out);
-		tableRow("User Principal", request.getUserPrincipal(), out);
+    // HTTP
 
-		endTable(out);
-	}
+    tableRow("<b>Method</b>", request.getMethod(), out);
+    tableRow("Auth Type", request.getAuthType(), out);
 
-	/**
-	 * Dumps information about a <code>ServletContext</code> to the
-	 * given <code>PrintWriter</code>
-	 */
-	private static void dump(ServletContext context, PrintWriter out) {
-		startTable("ServletContext", out);
+    for (Enumeration names = request.getHeaderNames(); names.hasMoreElements();) {
+      String name = (String) names.nextElement();
+      String value = request.getHeader(name);
+      tableRow("Header \"" + name + "\"", value, out);
+    }
 
-		tableRow("Context name", context.getServletContextName(), out);
-		tableRow("Server Info", context.getServerInfo(), out);
+    tableRow("Path Info", request.getPathInfo(), out);
+    tableRow("Path Translated", request.getPathTranslated(), out);
+    tableRow("Query String", request.getQueryString(), out);
+    tableRow("Remote User", request.getRemoteUser(), out);
+    tableRow("Requested Session ID", request.getRequestedSessionId(), out);
+    tableRow("Request URI", request.getRequestURI(), out);
+    tableRow("Request URL", request.getRequestURL(), out);
+    tableRow("Servlet Path", request.getServletPath(), out);
+    tableRow("User Principal", request.getUserPrincipal(), out);
 
-		for (Enumeration attrs = context.getAttributeNames(); attrs.hasMoreElements(); ) {
-			String name = (String) attrs.nextElement();
-			Object value = context.getAttribute(name);
-			tableRow("Attribute \"" + name + "\"", String.valueOf(value), out);
-		}
+    endTable(out);
+  }
 
-		for (Enumeration initParams = context.getInitParameterNames(); initParams.hasMoreElements(); ) {
-			String name = (String) initParams.nextElement();
-			String param = context.getInitParameter(name);
-			tableRow("Init parameter \"" + name + "\"", param, out);
-		}
+  /**
+   * Dumps information about a <code>ServletContext</code> to the
+   * given <code>PrintWriter</code>
+   */
+  private static void dump(ServletContext context, PrintWriter out) {
+    startTable("ServletContext", out);
 
-		tableRow("Major version", String.valueOf(context.getMajorVersion()), out);
-		tableRow("Minor version", String.valueOf(context.getMinorVersion()), out);
+    tableRow("Context name", context.getServletContextName(), out);
+    tableRow("Server Info", context.getServerInfo(), out);
+
+    for (Enumeration attrs = context.getAttributeNames(); attrs.hasMoreElements();) {
+      String name = (String) attrs.nextElement();
+      Object value = context.getAttribute(name);
+      tableRow("Attribute \"" + name + "\"", String.valueOf(value), out);
+    }
+
+    for (Enumeration initParams = context.getInitParameterNames(); initParams.hasMoreElements();) {
+      String name = (String) initParams.nextElement();
+      String param = context.getInitParameter(name);
+      tableRow("Init parameter \"" + name + "\"", param, out);
+    }
+
+    tableRow("Major version", String.valueOf(context.getMajorVersion()), out);
+    tableRow("Minor version", String.valueOf(context.getMinorVersion()), out);
 
 
-		endTable(out);
-	}
+    endTable(out);
+  }
 
 }
