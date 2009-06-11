8cec15251e0a26e52fa058c602a7fedcce6215e4
David Whitlock <david.m.whitlock@gmail.com>
2009-06-11 05:15:22 +0000

    Use the codehaus Mojo GWT plugin

--- gwt/src/main/java/edu/pdx/cs399J/gwt/hello/client/Hello.java
+++ gwt/src/main/java/edu/pdx/cs399J/gwt/hello/client/Hello.java
@@ -1,8 +1,11 @@
 package edu.pdx.cs399J.gwt.hello.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * A basic GWT class
@@ -10,6 +13,12 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class Hello implements EntryPoint {
   public void onModuleLoad() {
     Button button = new Button("Hello World");
+    button.addClickListener(new ClickListener() {
+		public void onClick(Widget arg0) {
+			Window.alert("Hello from GWT");
+		}
+    	
+    });
     RootPanel.get().add(button);
   }
 }
