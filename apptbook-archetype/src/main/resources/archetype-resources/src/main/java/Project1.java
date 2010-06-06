dfacc4f751d8b5f91098e3a5d21e69545ec09522
David Whitlock <david.m.whitlock@gmail.com>
2010-06-06 04:53:46 +0000

    Added a Maven archetype for the Appointment Book Project

---
+++ apptbook-archetype/src/main/resources/archetype-resources/src/main/java/Project1.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+ * The main class for CS399J Project 1
+ */
+public class Project1 {
+
+  public static void main(String[] args) {
+    System.err.println("Missing command line arguments");
+    System.exit(1);
+  }
+
+}
