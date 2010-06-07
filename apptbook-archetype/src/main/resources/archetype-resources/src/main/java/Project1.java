59a3933eb98b45c62b2ef729f392f49f539932ab
David Whitlock <david.m.whitlock@gmail.com>
2010-06-07 03:44:05 +0000

    Updated the Archetype for the Appointment Book project to include more reporting

--- apptbook-archetype/src/main/resources/archetype-resources/src/main/java/Project1.java
+++ apptbook-archetype/src/main/resources/archetype-resources/src/main/java/Project1.java
@@ -3,12 +3,15 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
+import edu.pdx.cs399J.AbstractAppointmentBook;
+
 /**
- * The main class for CS399J Project 1
+ * The main class for the CS399J appointment book Project
  */
 public class Project1 {
 
   public static void main(String[] args) {
+    AbstractAppointmentBook book;
     System.err.println("Missing command line arguments");
     System.exit(1);
   }
