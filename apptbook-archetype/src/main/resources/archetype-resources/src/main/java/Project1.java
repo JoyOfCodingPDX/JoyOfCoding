96551debdcdf770216237932d1795934b504b604
David Whitlock <david.m.whitlock@gmail.com>
2010-06-26 04:56:53 +0000

    Make the archetype generate an "executable" jar file.

--- apptbook-archetype/src/main/resources/archetype-resources/src/main/java/Project1.java
+++ apptbook-archetype/src/main/resources/archetype-resources/src/main/java/Project1.java
@@ -11,7 +11,7 @@ import edu.pdx.cs399J.AbstractAppointmentBook;
 public class Project1 {
 
   public static void main(String[] args) {
-    AbstractAppointmentBook book;
+    Class c = AbstractAppointmentBook.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
     System.err.println("Missing command line arguments");
     for (String arg : args) {
       System.out.println(arg);
