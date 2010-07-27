c8874beda4def74002492520d07a7a62e43dd3bd
David Whitlock <david.m.whitlock@gmail.com>
2010-07-27 05:45:50 +0000

    Renamed GWT archetype

---
+++ apptbook-gwt-archetype/src/main/resources/archetype-resources/src/main/java/client/Project6.java
@@ -0,0 +1,52 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import edu.pdx.cs399J.AbstractPhoneBill;
+import edu.pdx.cs399J.AbstractPhoneCall;
+
+import java.util.Collection;
+
+/**
+ * A basic GWT class
+ */
+public class Project6 implements EntryPoint {
+  public void onModuleLoad() {
+    Button button = new Button("Ping Server");
+    button.addClickHandler(new ClickHandler() {
+        public void onClick( ClickEvent clickEvent )
+        {
+            PingServiceAsync async = GWT.create( PingService.class );
+            async.ping( new AsyncCallback<AbstractPhoneBill>() {
+
+                public void onFailure( Throwable ex )
+                {
+                    Window.alert(ex.toString());
+                }
+
+                public void onSuccess( AbstractPhoneBill bill )
+                {
+                    StringBuilder sb = new StringBuilder( bill.toString() );
+                    Collection<AbstractPhoneCall> calls = bill.getPhoneCalls();
+                    for ( AbstractPhoneCall call : calls ) {
+                        sb.append(call);
+                        sb.append("${symbol_escape}n");
+                    }
+                    Window.alert( sb.toString() );
+                }
+            });
+        }
+    });
+      RootPanel rootPanel = RootPanel.get();
+      rootPanel.add(button);
+  }
+}
