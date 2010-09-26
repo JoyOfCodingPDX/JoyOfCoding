80a13bf0778539af0cb476d7c798ac85c29a7c83
David Whitlock <david.m.whitlock@gmail.com>
2010-09-26 05:33:04 +0000

    Added a suite of GWT tests

---
+++ gwt/src/it/java/edu/pdx/cs399J/gwt/client/ExamplesGwtTestSuite.java
@@ -0,0 +1,22 @@
+package edu.pdx.cs399J.gwt.client;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * A suite of GWT tests that can all be run inside the same development mode shell instance.
+ * Making this a <code>TestCase</code> makes the GWT Maven plugin happy
+ */
+public class ExamplesGwtTestSuite extends TestCase {
+
+  public static Test suite() {
+    GWTTestSuite suite = new GWTTestSuite("GWT Examples");
+
+    suite.addTestSuite(DivisionServiceGwtTest.class);
+    suite.addTestSuite(DivisionServiceExampleGwtTest.class);
+
+    return suite;
+  }
+
+}
