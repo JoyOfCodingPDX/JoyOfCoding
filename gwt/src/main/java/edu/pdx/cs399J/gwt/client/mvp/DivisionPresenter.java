2e7326da1599135a8ce08f8c1b86a5a99aef7421
David Whitlock <david.m.whitlock@gmail.com>
2010-09-26 22:17:52 +0000

    Reformat source code

--- gwt/src/main/java/edu/pdx/cs399J/gwt/client/mvp/DivisionPresenter.java
+++ gwt/src/main/java/edu/pdx/cs399J/gwt/client/mvp/DivisionPresenter.java
@@ -14,145 +14,142 @@ import edu.pdx.cs399J.gwt.client.DivisionServiceAsync;
  * @author David Whitlock
  * @since Summer 2010
  */
-public class DivisionPresenter
-{
-    /** The view for this presenter */
-    private final Display view;
+public class DivisionPresenter {
+  /**
+   * The view for this presenter
+   */
+  private final Display view;
 
-    /** The remote service for performing division */
-    private final DivisionServiceAsync service;
+  /**
+   * The remote service for performing division
+   */
+  private final DivisionServiceAsync service;
 
-    /** The divisor */
-    private Integer divisor = null;
+  /**
+   * The divisor
+   */
+  private Integer divisor = null;
 
-    /** The dividend*/
-    private Integer dividend = null;
+  /**
+   * The dividend
+   */
+  private Integer dividend = null;
 
-    @VisibleForTesting
-    Integer getDividend()
-    {
-        return this. dividend;
-    }
+  @VisibleForTesting
+  Integer getDividend() {
+    return this.dividend;
+  }
 
-    @VisibleForTesting
-    Integer getDivisor()
-    {
-        return divisor;
-    }
+  @VisibleForTesting
+  Integer getDivisor() {
+    return divisor;
+  }
 
-    @VisibleForTesting
-    void setDivisor( int divisor )
-    {
-        this.divisor = divisor;
-    }
+  @VisibleForTesting
+  void setDivisor(int divisor) {
+    this.divisor = divisor;
+  }
 
-    @VisibleForTesting
-    void setDividend( int dividend )
-    {
-        this.dividend = dividend;
-    }
+  @VisibleForTesting
+  void setDividend(int dividend) {
+    this.dividend = dividend;
+  }
 
 
-    /**
-     * The view interface for the division UI
-     */
-    public interface Display {
-        void setDivisorChangeHandler( ValueChangeHandler<String> handler );
+  /**
+   * The view interface for the division UI
+   */
+  public interface Display {
+    void setDivisorChangeHandler(ValueChangeHandler<String> handler);
 
-        public void setDivisorValid( boolean valid );
+    public void setDivisorValid(boolean valid);
 
-        void setDividendChangeHandler( ValueChangeHandler<String> handler );
+    void setDividendChangeHandler(ValueChangeHandler<String> handler);
 
-        public void setDividendValid( boolean valid );
+    public void setDividendValid(boolean valid);
 
-        public void setDivisionEnabled( boolean enabled );
+    public void setDivisionEnabled(boolean enabled);
 
-        void setDivisionClickHandler( ClickHandler handler);
+    void setDivisionClickHandler(ClickHandler handler);
 
-        void setErrorMessage( String message );
+    void setErrorMessage(String message);
 
-        void setQuotient( String quotient );
-    }
+    void setQuotient(String quotient);
+  }
 
-    public DivisionPresenter( Display view, DivisionServiceAsync service ) {
+  public DivisionPresenter(Display view, DivisionServiceAsync service) {
 
-        this.view = view;
-        this.service = service;
-        
-        this.bind();
-    }
+    this.view = view;
+    this.service = service;
 
-    public void bind() {
-       this.view.setDivisionEnabled( false );
-        
-       this.view.setDividendChangeHandler( new ValueChangeHandler<String>() {
-           public void onValueChange( ValueChangeEvent<String> change )
-           {
-               try {
-                 DivisionPresenter.this.dividend = Integer.parseInt(change.getValue());
+    this.bind();
+  }
 
-               } catch (NumberFormatException ex) {
-                 DivisionPresenter.this.dividend = null;
-               }
+  public void bind() {
+    this.view.setDivisionEnabled(false);
 
-               updateView();
-           }
-       });
+    this.view.setDividendChangeHandler(new ValueChangeHandler<String>() {
+      public void onValueChange(ValueChangeEvent<String> change) {
+        try {
+          DivisionPresenter.this.dividend = Integer.parseInt(change.getValue());
 
-      this.view.setDivisorChangeHandler( new ValueChangeHandler<String>() {
+        } catch (NumberFormatException ex) {
+          DivisionPresenter.this.dividend = null;
+        }
 
-          public void onValueChange( ValueChangeEvent<String> event )
-          {
-              try {
-                DivisionPresenter.this.divisor = Integer.parseInt(event.getValue());
+        updateView();
+      }
+    });
 
-              } catch (NumberFormatException ex) {
-                DivisionPresenter.this.divisor = null;
-              }
+    this.view.setDivisorChangeHandler(new ValueChangeHandler<String>() {
 
-              updateView();
+      public void onValueChange(ValueChangeEvent<String> event) {
+        try {
+          DivisionPresenter.this.divisor = Integer.parseInt(event.getValue());
+
+        } catch (NumberFormatException ex) {
+          DivisionPresenter.this.divisor = null;
+        }
+
+        updateView();
+      }
+    });
+
+    this.view.setDivisionClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent clickEvent) {
+        if (divisor == null) {
+          throw new RuntimeException("Shouldn't be able to perform division with a null divisor");
+        }
+
+        if (dividend == null) {
+          throw new RuntimeException("Shouldn't be able to perform division with a null dividend");
+        }
+
+        service.divide(dividend, divisor, new AsyncCallback<Integer>() {
+          public void onFailure(Throwable failure) {
+            view.setErrorMessage(failure.toString());
           }
-      });
 
-      this.view.setDivisionClickHandler( new ClickHandler() {
-          public void onClick( ClickEvent clickEvent )
-          {
-              if (divisor == null) {
-                  throw new RuntimeException("Shouldn't be able to perform division with a null divisor");
-              }
-
-              if (dividend == null) {
-                  throw new RuntimeException("Shouldn't be able to perform division with a null dividend");
-              }
-
-              service.divide( dividend, divisor, new AsyncCallback<Integer>() {
-                  public void onFailure( Throwable failure )
-                  {
-                      view.setErrorMessage(failure.toString());
-                  }
-
-                  public void onSuccess( Integer quotient )
-                  {
-                      view.setQuotient(String.valueOf(quotient));
-                  }
-              });
+          public void onSuccess(Integer quotient) {
+            view.setQuotient(String.valueOf(quotient));
           }
-      });
-    }
+        });
+      }
+    });
+  }
 
-    /**
-     * Updates the state of the view based on the values of the dividend and the divisor
-     */
-    private void updateView()
-    {
-        boolean dividendValid = (dividend != null);
-        view.setDividendValid( dividendValid );
+  /**
+   * Updates the state of the view based on the values of the dividend and the divisor
+   */
+  private void updateView() {
+    boolean dividendValid = (dividend != null);
+    view.setDividendValid(dividendValid);
 
-        boolean divisorValid = (divisor != null);
-        view.setDivisorValid( divisorValid );
+    boolean divisorValid = (divisor != null);
+    view.setDivisorValid(divisorValid);
 
-        view.setDivisionEnabled( dividendValid && divisorValid );
+    view.setDivisionEnabled(dividendValid && divisorValid);
 
-        view.setQuotient( "" );
-    }
+    view.setQuotient("");
+  }
 }
