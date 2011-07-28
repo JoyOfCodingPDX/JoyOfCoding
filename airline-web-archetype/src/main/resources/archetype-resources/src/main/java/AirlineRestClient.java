64f790dfedb9f03afbc0d1ae3c31d42024551688
David Whitlock <david.m.whitlock@gmail.com>
2011-07-28 00:30:08 +0000

    Add a comment instructing the student to make changes to this code.

--- airline-web-archetype/src/main/resources/archetype-resources/src/main/java/AirlineRestClient.java
+++ airline-web-archetype/src/main/resources/archetype-resources/src/main/java/AirlineRestClient.java
@@ -8,7 +8,9 @@ import edu.pdx.cs410J.web.HttpRequestHelper;
 import java.io.IOException;
 
 /**
- * A helper class for accessing the rest client
+ * A helper class for accessing the rest client.  Note that this class provides
+ * an example of how to make gets and posts to a URL.  You'll need to change it
+ * to do something other than just send key/value pairs.
  */
 public class AirlineRestClient extends HttpRequestHelper
 {
