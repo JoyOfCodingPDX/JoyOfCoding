477b867a7453c82e3ed4de5a8c5f99cba43d50bf
David Whitlock <david.m.whitlock@gmail.com>
2012-05-25 04:38:53 +0000

    Added archetype for the Project 1 Phone Bill application

---
+++ phonebill-archetype/src/main/resources/archetype-resources/src/main/java/Project1.java
@@ -0,0 +1,22 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+
+/**
+ * The main class for the CS410J Phone Bill Project
+ */
+public class Project1 {
+
+  public static void main(String[] args) {
+    Class c = AbstractPhoneBill.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    System.err.println("Missing command line arguments");
+    for (String arg : args) {
+      System.out.println(arg);
+    }
+    System.exit(1);
+  }
+
+}
---
+++ phonebill-archetype/src/main/resources/archetype-resources/src/test/java/Project1Test.java
@@ -0,0 +1,33 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import edu.pdx.cs410J.InvokeMainTestCase;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Tests the functionality in the {@link Project1} main class.
+ */
+public class Project1Test extends InvokeMainTestCase {
+
+    /**
+     * Invokes the main method of {@link Project1} with the given arguments.
+     */
+    private MainMethodResult invokeMain(String... args) {
+        return invokeMain( Project1.class, args );
+    }
+
+  /**
+   * Tests that invoking the main method with no arguments issues an error
+   */
+  @Test
+  public void testNoCommandLineArguments() {
+    MainMethodResult result = invokeMain();
+    assertEquals(new Integer(1), result.getExitCode());
+    assertTrue(result.getErr().contains( "Missing command line arguments" ));
+  }
+
+}
