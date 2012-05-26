d7c1cb2072929b5b62cb69d0fbdda5bfb0f8506d
David Whitlock <david.m.whitlock@gmail.com>
2012-05-26 02:39:47 +0000

    Added archetype for the REST Phone Bill application

---
+++ phonebill-web-archetype/src/main/resources/archetype-resources/src/main/java/PhoneBillServlet.java
@@ -0,0 +1,106 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneBillServlet extends HttpServlet
+{
+    private final Map<String, String> data = new HashMap<String, String>();
+
+    @Override
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+    {
+        response.setContentType( "text/plain" );
+
+        String key = getParameter( "key", request );
+        if (key != null) {
+            writeValue(key, response);
+
+        } else {
+            writeAllMappings(response);
+        }
+    }
+
+    @Override
+    protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+    {
+        response.setContentType( "text/plain" );
+
+        String key = getParameter( "key", request );
+        if (key == null) {
+            missingRequiredParameter( response, key );
+            return;
+        }
+
+        String value = getParameter( "value", request );
+        if ( value == null) {
+            missingRequiredParameter( response, value );
+            return;
+        }
+
+        this.data.put(key, value);
+
+        PrintWriter pw = response.getWriter();
+        pw.println(Messages.mappedKeyValue(key, value));
+        pw.flush();
+
+        response.setStatus( HttpServletResponse.SC_OK);
+    }
+
+    private void missingRequiredParameter( HttpServletResponse response, String key )
+        throws IOException
+    {
+        PrintWriter pw = response.getWriter();
+        pw.println( Messages.missingRequiredParameter(key));
+        pw.flush();
+        
+        response.setStatus( HttpServletResponse.SC_PRECONDITION_FAILED );
+    }
+
+    private void writeValue( String key, HttpServletResponse response ) throws IOException
+    {
+        String value = this.data.get(key);
+
+        PrintWriter pw = response.getWriter();
+        pw.println(Messages.getMappingCount( value != null ? 1 : 0 ));
+        pw.println(Messages.formatKeyValuePair( key, value ));
+
+        pw.flush();
+
+        response.setStatus( HttpServletResponse.SC_OK );
+    }
+
+    private void writeAllMappings( HttpServletResponse response ) throws IOException
+    {
+        PrintWriter pw = response.getWriter();
+        pw.println(Messages.getMappingCount( data.size() ));
+
+        for (Map.Entry<String, String> entry : this.data.entrySet()) {
+            pw.println(Messages.formatKeyValuePair(entry.getKey(), entry.getValue()));
+        }
+
+        pw.flush();
+
+        response.setStatus( HttpServletResponse.SC_OK );
+    }
+
+    private String getParameter(String name, HttpServletRequest request) {
+      String value = request.getParameter(name);
+      if (value == null || "".equals(value)) {
+        return null;
+
+      } else {
+        return value;
+      }
+    }
+
+}
