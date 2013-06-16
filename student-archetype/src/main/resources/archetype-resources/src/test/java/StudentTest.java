d854abe3a138a87be46eec854171cfadb1e2130f
David Whitlock <notmyrealaddress@email.com>
2013-06-16 14:33:56 -0700

    Initial Maven archetype for the Student sample project.

---
+++ student-archetype/src/main/resources/archetype-resources/src/test/java/StudentTest.java
@@ -0,0 +1,34 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+
+/**
+ * JUnit tests for the Student class.  These tests extend <code>InvokeMainTestCase</code>
+ * which allows them to easily invoke the <code>main</code> method of <code>Student</code>.
+ * They also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>matchers
+ * for more readable assertion statements.
+ */
+public class StudentTest extends InvokeMainTestCase
+{
+
+  @Test
+  public void invokingMainWithNoArgumentsHasExitCodeOf1() {
+    MainMethodResult result = invokeMain(Student.class);
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+
+  @Test
+  public void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    MainMethodResult result = invokeMain(Student.class);
+    assertThat(result.getErr(), containsString("Missing command line arguments"));
+  }
+
+}
