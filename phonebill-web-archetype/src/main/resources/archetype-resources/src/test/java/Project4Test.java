56280605d4a1c48894a7ff186fadd9a4488b1d73
David Whitlock <notmyrealaddress@email.com>
2014-04-05 11:20:29 -0700

    Created an uber jar and got the integration tests for the phonebill-web archetype working again.

--- phonebill-web-archetype/src/main/resources/archetype-resources/src/test/java/Project4Test.java
+++ phonebill-web-archetype/src/main/resources/archetype-resources/src/test/java/Project4Test.java
@@ -4,61 +4,65 @@
 package ${package};
 
 import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * Tests the {@link Project4} class by invoking its main method with various arguments 
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Project4Test extends InvokeMainTestCase {
     private static final String HOSTNAME = "localhost";
     private static final String PORT = String.valueOf(8080);
 
     @Test
-    public void testNoCommandLineArguments() {
+    public void test1NoCommandLineArguments() {
         MainMethodResult result = invokeMain( Project4.class );
-        assertEquals(new Integer(1), result.getExitCode());
-        assertTrue(result.getErr().contains( Project4.MISSING_ARGS ));
+        assertThat(result.getExitCode(), equalTo(1));
+        assertThat(result.getErr(), containsString(Project4.MISSING_ARGS));
     }
 
     @Test
-    public void testEmptyServer() {
+    public void test2EmptyServer() {
         MainMethodResult result = invokeMain( Project4.class, HOSTNAME, PORT );
-        assertEquals(result.getErr(), new Integer(0), result.getExitCode());
+        assertThat(result.getErr(), result.getExitCode(), equalTo(0));
         String out = result.getOut();
-        assertTrue( out, out.contains( Messages.getMappingCount(0)));
+        assertThat(out, out, containsString(Messages.getMappingCount(0)));
     }
 
     @Test
-    public void testNoValues() {
+    public void test3NoValues() {
         String key = "KEY";
         MainMethodResult result = invokeMain( Project4.class, HOSTNAME, PORT, key );
-        assertEquals(result.getErr(), new Integer(0), result.getExitCode());
+        assertThat(result.getErr(), result.getExitCode(), equalTo(0));
         String out = result.getOut();
-        assertTrue( out, out.contains( Messages.getMappingCount(0)));
-        assertTrue( out, out.contains( Messages.formatKeyValuePair( key, null )));
+        assertThat(out, out, containsString(Messages.getMappingCount(0)));
+        assertThat(out, out, containsString(Messages.formatKeyValuePair(key, null)));
     }
 
     @Test
-    public void testAddValue() {
+    public void test4AddValue() {
         String key = "KEY";
         String value = "VALUE";
 
         MainMethodResult result = invokeMain( Project4.class, HOSTNAME, PORT, key, value );
-        assertEquals(result.getErr(), new Integer(0), result.getExitCode());
+        assertThat(result.getErr(), result.getExitCode(), equalTo(0));
         String out = result.getOut();
-        assertTrue( out, out.contains( Messages.mappedKeyValue( key, value )));
+        assertThat(out, out, containsString(Messages.mappedKeyValue(key, value)));
 
         result = invokeMain( Project4.class, HOSTNAME, PORT, key );
         out = result.getOut();
-        assertTrue( out, out.contains( Messages.getMappingCount(1)));
-        assertTrue( out, out.contains( Messages.formatKeyValuePair( key, value )));
+        assertThat(out, out, containsString(Messages.getMappingCount(1)));
+        assertThat(out, out, containsString(Messages.formatKeyValuePair(key, value)));
 
         result = invokeMain( Project4.class, HOSTNAME, PORT );
         out = result.getOut();
-        assertTrue( out, out.contains( Messages.getMappingCount(1)));
-        assertTrue( out, out.contains( Messages.formatKeyValuePair( key, value )));
+        assertThat(out, out, containsString(Messages.getMappingCount(1)));
+        assertThat(out, out, containsString(Messages.formatKeyValuePair(key, value)));
     }
 }
