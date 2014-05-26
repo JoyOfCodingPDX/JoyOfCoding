d50eaab1997a178b8adb9aaaf8242fef52b6ad35
David Whitlock <notmyrealaddress@email.com>
2014-05-25 19:45:44 -0700

    Get information about the Grader's INBOX.

--- grader/src/main/java/edu/pdx/cs410J/grader/GraderEmailAccount.java
+++ grader/src/main/java/edu/pdx/cs410J/grader/GraderEmailAccount.java
@@ -1,8 +1,10 @@
 package edu.pdx.cs410J.grader;
 
+import javax.mail.Folder;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Store;
+import java.io.PrintStream;
 import java.util.Properties;
 
 public class GraderEmailAccount {
@@ -15,6 +17,48 @@ public class GraderEmailAccount {
 
   public void fetchProjectSubmissions() {
     Store store = connectToGmail();
+    Folder folder = openProjectSubmissionsFolder(store);
+    printFolderInformation(folder);
+
+
+    try {
+      folder.close(false);
+      store.close();
+
+    } catch (MessagingException ex) {
+      throw new IllegalStateException("While closing folder and store", ex);
+    }
+  }
+
+  private void printFolderInformation(Folder folder) {
+    try {
+      PrintStream out = System.out;
+      out.println("Folder: " + folder.getFullName());
+      out.println("Message count: " + folder.getMessageCount());
+      out.println("Unread messages: " + folder.getUnreadMessageCount());
+
+    } catch (MessagingException ex) {
+      throw new IllegalStateException("While getting folder information", ex);
+    }
+  }
+
+  private Folder openProjectSubmissionsFolder(Store store) {
+    try {
+      Folder folder = store.getDefaultFolder();
+      checkForValidFolder(folder, "Default");
+      folder = folder.getFolder("INBOX");
+      folder.open(Folder.READ_ONLY);
+      return folder;
+
+    } catch (MessagingException ex) {
+      throw new IllegalStateException("While opening project submissions folder", ex);
+    }
+  }
+
+  private void checkForValidFolder(Folder folder, String folderName) {
+    if (folder == null) {
+      throw new IllegalStateException("Folder \"" + folderName + "\" does not exist");
+    }
 
   }
 
