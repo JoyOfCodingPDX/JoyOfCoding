015c2c22af27a7cc1edd63686c670ca1d45bc308
David Whitlock <notmyrealaddress@email.com>
2014-05-31 15:18:53 -0700

    Download attachments into a directory specified on the command line.

--- grader/src/main/java/edu/pdx/cs410J/grader/FetchAndProcessGraderEmail.java
+++ grader/src/main/java/edu/pdx/cs410J/grader/FetchAndProcessGraderEmail.java
@@ -1,19 +1,33 @@
 package edu.pdx.cs410J.grader;
 
+import com.google.common.io.ByteStreams;
+
 import java.io.*;
 
 public class FetchAndProcessGraderEmail {
 
   public static void main(String[] args) {
     String passwordFile = null;
+    String directoryName = null;
     for (String arg : args) {
       if (passwordFile == null) {
         passwordFile = arg;
+
+
+      } else if (directoryName == null) {
+        directoryName = arg;
+
+      } else {
+        usage("Extraneous argument: " + arg);
       }
     }
 
     if (passwordFile == null) {
       usage("Missing password file");
+    }
+
+    if (directoryName == null) {
+      usage("Missing directory name");
     }
 
     String password = null;
@@ -24,6 +38,16 @@ public class FetchAndProcessGraderEmail {
       usage("Exception while reading password file");
     }
 
+    assert directoryName != null;
+    final File directory = new File(directoryName);
+    if (!directory.exists()) {
+      usage("Download directory \"" + directoryName + "\" does not exist");
+    }
+
+    if (!directory.isDirectory()) {
+      usage("Download directory \"" + directoryName + "\" is not a directory");
+    }
+
     GraderEmailAccount account = new GraderEmailAccount(password);
     account.fetchAttachmentsFromUnreadMessagesInFolder("Project Submissions", new EmailAttachmentProcessor() {
 
@@ -31,6 +55,31 @@ public class FetchAndProcessGraderEmail {
       public void processAttachment(String fileName, InputStream inputStream) {
         System.out.println("    File name: " + fileName);
         System.out.println("    InputStream: " + inputStream);
+
+        try {
+          writeToFileToDirectory(directory, fileName, inputStream);
+        } catch (IOException ex) {
+          logException("While writing \"" + fileName + "\" to \"" + directory + "\"", ex);
+        }
+      }
+
+      private void logException(String message, IOException ex) {
+        System.err.println(message);
+        ex.printStackTrace(System.err);
+      }
+
+      private void writeToFileToDirectory(File directory, String fileName, InputStream inputStream) throws IOException {
+        File file = new File(directory, fileName);
+
+        if (file.exists()) {
+          warnOfPreExistingFile(file);
+        }
+
+        ByteStreams.copy(inputStream, new FileOutputStream(file));
+      }
+
+      private void warnOfPreExistingFile(File file) {
+        System.out.println("File \"" + file + "\" already exists");
       }
     });
   }
@@ -49,8 +98,9 @@ public class FetchAndProcessGraderEmail {
     PrintStream err = System.err;
     err.println("** " + message);
     err.println();
-    err.println("FetchAndProcessGraderEmail passwordFile");
+    err.println("FetchAndProcessGraderEmail passwordFile directory");
     err.println("  passwordFile   File containing the password for the Grader's email account");
+    err.println("  directory      The directory in which to download attachments");
     err.println();
 
     System.exit(1);
