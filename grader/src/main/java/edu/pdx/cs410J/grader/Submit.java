28ce742f402eae50706b22086d22aec8c606a5c2
Sean Mahan <sean97140@users.noreply.github.com>
2014-07-14 19:15:17 -0700

    this will work
    use IntelliJ for future work, not Notepad++

--- grader/src/main/java/edu/pdx/cs410J/grader/Submit.java
+++ grader/src/main/java/edu/pdx/cs410J/grader/Submit.java
@@ -245,7 +245,7 @@ public class Submit {
       String s = "No source files were found.";
       throw new IllegalStateException(s);
     } else if (sourceFiles.size() < 3) {
-      String s = "Too few source files were submitted. Each project requires at least 3 files be" 
+      String s = "Too few source files were submitted. Each project requires at least 3 files be" + 
                  " submitted. You only submitted " + sourceFiles.size() + " files";
       throw new IllegalStateException(s);
     }
