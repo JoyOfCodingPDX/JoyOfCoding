a3cf6dd8d9ca78fa6d0c8fb2fdfe7f1928c988b4
David Whitlock <notmyrealaddress@email.com>
2014-10-25 10:47:06 -0700

    Add API documentation for the AirlineServlet in order to address issue #72 for the airline REST project archetype.

--- airline-web-archetype/src/main/resources/archetype-resources/src/main/java/AirlineServlet.java
+++ airline-web-archetype/src/main/resources/archetype-resources/src/main/java/AirlineServlet.java
@@ -12,10 +12,20 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AirlineServlet extends HttpServlet
-{
-  private final Map<String, String> data = new HashMap<String, String>();
+/**
+ * This servlet ultimately provides a REST API for working with an
+ * <code>Airline</code>.  However, in its current state, it is an example
+ * of how to use HTTP and Java servlets to store simple key/value pairs.
+ */
+public class AirlineServlet extends HttpServlet {
+  private final Map<String, String> data = new HashMap<>();
 
+  /**
+   * Handles an HTTP GET request from a client by writing the value of the key
+   * specified in the "key" HTTP parameter to the HTTP response.  If the "key"
+   * parameter is not specified, all of the key/value pairs are written to the
+   * HTTP response.
+   */
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/plain");
@@ -29,19 +39,24 @@ public class AirlineServlet extends HttpServlet
     }
   }
 
+  /**
+   * Handles an HTTP POST request by storing the key/value pair specified by the
+   * "key" and "value" request parameters.  It writes the key/value pair to the
+   * HTTP response.
+   */
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/plain");
 
     String key = getParameter("key", request);
     if (key == null) {
-      missingRequiredParameter(response, key);
+      missingRequiredParameter(response, "key");
       return;
     }
 
     String value = getParameter("value", request);
     if (value == null) {
-      missingRequiredParameter(response, value);
+      missingRequiredParameter(response, "value");
       return;
     }
 
@@ -54,15 +69,26 @@ public class AirlineServlet extends HttpServlet
     response.setStatus(HttpServletResponse.SC_OK);
   }
 
-  private void missingRequiredParameter(HttpServletResponse response, String key)
+  /**
+   * Writes an error message about a missing parameter to the HTTP response.
+   *
+   * The text of the error message is created by {@link Messages${symbol_pound}missingRequiredParameter(String)}
+   */
+  private void missingRequiredParameter(HttpServletResponse response, String parameterName)
     throws IOException {
     PrintWriter pw = response.getWriter();
-    pw.println(Messages.missingRequiredParameter(key));
+    pw.println(Messages.missingRequiredParameter(parameterName));
     pw.flush();
 
     response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
   }
 
+  /**
+   * Writes the value of the given key to the HTTP response.
+   *
+   * The text of the message is formatted with {@link Messages${symbol_pound}getMappingCount(int)}
+   * and {@link Messages${symbol_pound}formatKeyValuePair(String, String)}
+   */
   private void writeValue(String key, HttpServletResponse response) throws IOException {
     String value = this.data.get(key);
 
@@ -75,6 +101,12 @@ public class AirlineServlet extends HttpServlet
     response.setStatus(HttpServletResponse.SC_OK);
   }
 
+  /**
+   * Writes all of the key/value pairs to the HTTP response.
+   *
+   * The text of the message is formatted with
+   * {@link Messages${symbol_pound}formatKeyValuePair(String, String)}
+   */
   private void writeAllMappings(HttpServletResponse response) throws IOException {
     PrintWriter pw = response.getWriter();
     pw.println(Messages.getMappingCount(data.size()));
@@ -88,6 +120,12 @@ public class AirlineServlet extends HttpServlet
     response.setStatus(HttpServletResponse.SC_OK);
   }
 
+  /**
+   * Returns the value of the HTTP request parameter with the given name.
+   *
+   * @return <code>null</code> if the value of the parameter is
+   *         <code>null</code> or is the empty string
+   */
   private String getParameter(String name, HttpServletRequest request) {
     String value = request.getParameter(name);
     if (value == null || "".equals(value)) {
