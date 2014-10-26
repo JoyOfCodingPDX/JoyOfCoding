3c6496459f8ad77d4aca78122e2b19682e098d2b
David Whitlock <notmyrealaddress@email.com>
2014-10-26 06:21:38 -0700

    Added javadocs to the methods of the servlets in the web project archetypes so that students better understand the implementation of the basic key/value pair application.  This addresses issue #72.

--- phonebill-web-archetype/src/main/resources/archetype-resources/src/main/java/PhoneBillServlet.java
+++ phonebill-web-archetype/src/main/resources/archetype-resources/src/main/java/PhoneBillServlet.java
@@ -12,10 +12,21 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This servlet ultimately provides a REST API for working with an
+ * <code>PhoneBill</code>.  However, in its current state, it is an example
+ * of how to use HTTP and Java servlets to store simple key/value pairs.
+ */
 public class PhoneBillServlet extends HttpServlet
 {
-    private final Map<String, String> data = new HashMap<String, String>();
+    private final Map<String, String> data = new HashMap<>();
 
+    /**
+     * Handles an HTTP GET request from a client by writing the value of the key
+     * specified in the "key" HTTP parameter to the HTTP response.  If the "key"
+     * parameter is not specified, all of the key/value pairs are written to the
+     * HTTP response.
+     */
     @Override
     protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
     {
@@ -30,6 +41,11 @@ public class PhoneBillServlet extends HttpServlet
         }
     }
 
+    /**
+     * Handles an HTTP POST request by storing the key/value pair specified by the
+     * "key" and "value" request parameters.  It writes the key/value pair to the
+     * HTTP response.
+     */
     @Override
     protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
     {
@@ -37,13 +53,13 @@ public class PhoneBillServlet extends HttpServlet
 
         String key = getParameter( "key", request );
         if (key == null) {
-            missingRequiredParameter( response, key );
+            missingRequiredParameter( response, "key" );
             return;
         }
 
         String value = getParameter( "value", request );
         if ( value == null) {
-            missingRequiredParameter( response, value );
+            missingRequiredParameter( response, "value" );
             return;
         }
 
@@ -56,16 +72,27 @@ public class PhoneBillServlet extends HttpServlet
         response.setStatus( HttpServletResponse.SC_OK);
     }
 
-    private void missingRequiredParameter( HttpServletResponse response, String key )
+    /**
+     * Writes an error message about a missing parameter to the HTTP response.
+     *
+     * The text of the error message is created by {@link Messages${symbol_pound}missingRequiredParameter(String)}
+     */
+    private void missingRequiredParameter( HttpServletResponse response, String parameterName )
         throws IOException
     {
         PrintWriter pw = response.getWriter();
-        pw.println( Messages.missingRequiredParameter(key));
+        pw.println( Messages.missingRequiredParameter(parameterName));
         pw.flush();
         
         response.setStatus( HttpServletResponse.SC_PRECONDITION_FAILED );
     }
 
+    /**
+     * Writes the value of the given key to the HTTP response.
+     *
+     * The text of the message is formatted with {@link Messages${symbol_pound}getMappingCount(int)}
+     * and {@link Messages${symbol_pound}formatKeyValuePair(String, String)}
+     */
     private void writeValue( String key, HttpServletResponse response ) throws IOException
     {
         String value = this.data.get(key);
@@ -79,6 +106,12 @@ public class PhoneBillServlet extends HttpServlet
         response.setStatus( HttpServletResponse.SC_OK );
     }
 
+    /**
+     * Writes all of the key/value pairs to the HTTP response.
+     *
+     * The text of the message is formatted with
+     * {@link Messages${symbol_pound}formatKeyValuePair(String, String)}
+     */
     private void writeAllMappings( HttpServletResponse response ) throws IOException
     {
         PrintWriter pw = response.getWriter();
@@ -93,6 +126,12 @@ public class PhoneBillServlet extends HttpServlet
         response.setStatus( HttpServletResponse.SC_OK );
     }
 
+    /**
+     * Returns the value of the HTTP request parameter with the given name.
+     *
+     * @return <code>null</code> if the value of the parameter is
+     *         <code>null</code> or is the empty string
+     */
     private String getParameter(String name, HttpServletRequest request) {
       String value = request.getParameter(name);
       if (value == null || "".equals(value)) {
