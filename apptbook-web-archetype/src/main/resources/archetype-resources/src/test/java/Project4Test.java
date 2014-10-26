7d66e4dff6e283fe05cf1ff3b12dc7584b8f3344
David Whitlock <notmyrealaddress@email.com>
2014-10-25 17:31:28 -0700

    Made the HTTP port that the REST archetypes use configurable.  This addresses issue #75.

--- apptbook-web-archetype/src/main/resources/archetype-resources/src/test/java/Project4Test.java
+++ apptbook-web-archetype/src/main/resources/archetype-resources/src/test/java/Project4Test.java
@@ -18,7 +18,7 @@ import static org.hamcrest.Matchers.equalTo;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Project4Test extends InvokeMainTestCase {
     private static final String HOSTNAME = "localhost";
-    private static final String PORT = String.valueOf(8080);
+    private static final String PORT = System.getProperty("http.port", "8080");
 
     @Test
     public void test1NoCommandLineArguments() {
