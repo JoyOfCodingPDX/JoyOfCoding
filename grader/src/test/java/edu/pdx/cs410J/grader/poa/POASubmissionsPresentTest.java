920532033066ffca1ce76fd63bed7c8dccfee31a
David Whitlock <david.m.whitlock@gmail.com>
2014-12-20 21:16:42 -0800

    Test that multiple POASubmission events are displayed in the order in which they are sent.

--- grader/src/test/java/edu/pdx/cs410J/grader/poa/POASubmissionsPresentTest.java
+++ grader/src/test/java/edu/pdx/cs410J/grader/poa/POASubmissionsPresentTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.Arrays;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -13,14 +13,13 @@ import static org.mockito.Mockito.verify;
 public class POASubmissionsPresentTest {
 
   private EventBus bus;
-  private POASubmissionsPresenter presenter;
   private POASubmissionsView view;
 
   @Before
   public void setUp() {
     bus = new EventBus();
     view = mock(POASubmissionsView.class);
-    presenter = new POASubmissionsPresenter(bus, view);
+    new POASubmissionsPresenter(bus, view);
   }
 
   @Test
@@ -32,7 +31,22 @@ public class POASubmissionsPresentTest {
     POASubmission submission = createPOASubmission(subject, submitter, submitTime);
     bus.post(submission);
 
-    verify(view).setPOASubmissionsDescriptions(Collections.singletonList(subject));
+    verify(view).setPOASubmissionsDescriptions(Arrays.asList(subject));
+  }
+
+  @Test
+  public void submissionsDescriptionsAreDisplayedInOrderThatEventsWereSent() {
+    String subject1 = "POA for Project 1";
+    String subject2 = "POA for Project 2";
+    String submitter = "Test Student";
+    LocalDateTime submitTime = LocalDateTime.now();
+
+    bus.post(createPOASubmission(subject1, submitter, submitTime));
+    bus.post(createPOASubmission(subject2, submitter, submitTime));
+
+    verify(view).setPOASubmissionsDescriptions(Arrays.asList(subject1));
+    verify(view).setPOASubmissionsDescriptions(Arrays.asList(subject1, subject2));
+
   }
 
   private POASubmission createPOASubmission(String subject, String submitter, LocalDateTime submitTime) {
