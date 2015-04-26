1219ee0b56818b764cc50accb14a698e64c10c81
David Whitlock <david.m.whitlock@gmail.com>
2015-04-26 10:18:26 -0700

    Test that the jar file to submit is created with the expected content.

--- submit-maven-plugin/src/it/simple-it/verify.groovy
+++ submit-maven-plugin/src/it/simple-it/verify.groovy
@@ -1,3 +1,15 @@
+import java.util.jar.JarFile
+
 File touchFile = new File( basedir, "target/touch.txt" );
 
 assert touchFile.isFile()
+
+File jarDir = new File( basedir, "target" )
+File jarFile = jarDir.listFiles().find{it.isFile() && it=~/.jar$/ }
+assert jarFile.exists()
+assert jarFile.isFile()
+
+def jarEntries = new JarFile(jarFile).entries()*.name
+assert jarEntries.contains("edu/pdx/cs410J/it/TestClass.java")
+assert jarEntries.contains("edu/pdx/cs410J/it/TestClass2.java")
+assert jarEntries.contains("edu/pdx/cs410J/it/TestClass3.java")
