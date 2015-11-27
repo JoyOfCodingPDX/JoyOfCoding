c0e7b89f5826067694c67b5fe94ad7817b069cd6
David Whitlock <david.m.whitlock@gmail.com>
2015-11-27 08:12:07 -0800

    Add integration tests and true unit tests to the appointment book project archetype.

---
+++ apptbook-archetype/src/main/resources/archetype-resources/src/it/java/Project1IT.java
@@ -0,0 +1,35 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Integration tests for the {@link Project1} main class.
+ */
+public class Project1IT extends InvokeMainTestCase {
+
+  /**
+   * Invokes the main method of {@link Project1} with the given arguments.
+   */
+  private MainMethodResult invokeMain(String... args) {
+    return invokeMain( Project1.class, args );
+  }
+
+  /**
+   * Tests that invoking the main method with no arguments issues an error
+   */
+  @Test
+  public void testNoCommandLineArguments() {
+    MainMethodResult result = invokeMain();
+    assertThat(result.getExitCode(), equalTo(1));
+    assertThat(result.getErr(), containsString("Missing command line arguments"));
+  }
+
+}
