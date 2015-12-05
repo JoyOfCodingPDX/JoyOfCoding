d22e05e57386968144eb677bd9f30b3ced836bdb
David Whitlock <david.m.whitlock@gmail.com>
2015-12-05 15:26:41 -0800

    Add an original version of the airline GWT application.

---
+++ projects-parent/originals-parent/airline-gwt/src/main/java/edu/pdx/cs410J/original/client/Airline.java
@@ -0,0 +1,24 @@
+package edu.pdx.cs410J.original.client;
+
+import edu.pdx.cs410J.AbstractFlight;
+import edu.pdx.cs410J.AbstractAirline;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Airline extends AbstractAirline
+{
+  private Collection<AbstractFlight> flights = new ArrayList<AbstractFlight>();
+
+  public String getName() {
+    return "Air CS410J";
+  }
+
+  public void addFlight(AbstractFlight flight) {
+    this.flights.add(flight);
+  }
+
+  public Collection getFlights() {
+    return this.flights;
+  }
+}
