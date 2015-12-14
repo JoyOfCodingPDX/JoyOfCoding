b28bbdac2b80266af2558f5ba283b8fa39a7e0bd
David Whitlock <david.m.whitlock@gmail.com>
2015-12-13 19:45:43 -0800

    Use GWT SyncProxy to remote invoke the DivisionService from an integration test.

--- gwt-parent/gwt/src/it/java/edu/pdx/cs410J/gwt/DivisionServiceSyncProxyIT.java
+++ gwt-parent/gwt/src/it/java/edu/pdx/cs410J/gwt/DivisionServiceSyncProxyIT.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410J.gwt;
 
+import com.gdevelop.gwt.syncrpc.SyncProxy;
+import edu.pdx.cs410J.gwt.client.DivisionService;
 import edu.pdx.cs410J.web.HttpRequestHelper;
 import org.junit.Test;
 
@@ -13,9 +15,22 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class DivisionServiceSyncProxyIT extends HttpRequestHelper {
 
+  private final int httpPort = Integer.getInteger("http.port", 8080);
+  private String webAppUrl = "http://localhost:" + httpPort + "/gwt";
+
   @Test
   public void gwtWebApplicationIsRunning() throws IOException {
-    Response response = get("http://localhost:8080/gwt");
+    Response response = get(this.webAppUrl);
     assertEquals(200, response.getCode());
+  }
+
+  @Test
+  public void canInvokeDivisionServiceWithGwtSyncProxy() {
+    String moduleName = "examples";
+    SyncProxy.setBaseURL(this.webAppUrl + "/" + moduleName + "/");
+
+    DivisionService sync = SyncProxy.createSync(DivisionService.class);
+    int quotient = sync.divide(6, 2);
+    assertEquals(3, quotient);
   }
 }
