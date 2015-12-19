29b4d7d92239a6d788848b79655ce9955a4b0806
David Whitlock <david.m.whitlock@gmail.com>
2015-12-18 22:01:44 -0800

    Added GWT Test Case integration tests for the GWT Phone Bill project.

--- gwt-parent/gwt-originals-parent/phonebill-gwt/src/main/java/edu/pdx/cs410J/original/client/PhoneBillGwt.java
+++ gwt-parent/gwt-originals-parent/phonebill-gwt/src/main/java/edu/pdx/cs410J/original/client/PhoneBillGwt.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs410J.original.client;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -9,7 +10,6 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 import edu.pdx.cs410J.AbstractPhoneCall;
-import edu.pdx.cs410J.AbstractPhoneBill;
 
 import java.util.Collection;
 
@@ -17,33 +17,64 @@ import java.util.Collection;
  * A basic GWT class that makes sure that we can send an Phone Bill back from the server
  */
 public class PhoneBillGwt implements EntryPoint {
-  public void onModuleLoad() {
-    Button button = new Button("Ping Server");
-    button.addClickHandler(new ClickHandler() {
-        public void onClick( ClickEvent clickEvent )
-        {
-            PingServiceAsync async = GWT.create( PingService.class );
-            async.ping( new AsyncCallback<PhoneBill>() {
+  private final Alerter alerter;
 
-                public void onFailure( Throwable ex )
-                {
-                    Window.alert(ex.toString());
-                }
+  @VisibleForTesting
+  Button button;
 
-                public void onSuccess( PhoneBill phonebill )
-                {
-                    StringBuilder sb = new StringBuilder( phonebill.toString() );
-                    Collection<AbstractPhoneCall> calls = phonebill.getPhoneCalls();
-                    for ( AbstractPhoneCall call : calls ) {
-                        sb.append(call);
-                        sb.append("\n");
-                    }
-                    Window.alert( sb.toString() );
-                }
-            });
-        }
+  public PhoneBillGwt() {
+    this(new Alerter() {
+      @Override
+      public void alert(String message) {
+        Window.alert(message);
+      }
     });
-      RootPanel rootPanel = RootPanel.get();
-      rootPanel.add(button);
   }
+
+  @VisibleForTesting
+  PhoneBillGwt(Alerter alerter) {
+    this.alerter = alerter;
+
+    addWidgets();
+  }
+
+  private void addWidgets() {
+    button = new Button("Ping Server");
+    button.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent clickEvent) {
+        PingServiceAsync async = GWT.create(PingService.class);
+        async.ping(new AsyncCallback<PhoneBill>() {
+
+          @Override
+          public void onFailure(Throwable ex) {
+            alerter.alert(ex.toString());
+          }
+
+          @Override
+          public void onSuccess(PhoneBill bill) {
+            StringBuilder sb = new StringBuilder(bill.toString());
+            Collection<AbstractPhoneCall> calls = bill.getPhoneCalls();
+            for (AbstractPhoneCall call : calls) {
+              sb.append(call);
+              sb.append("\n");
+            }
+            alerter.alert(sb.toString());
+          }
+        });
+      }
+    });
+  }
+
+  @Override
+  public void onModuleLoad() {
+    RootPanel rootPanel = RootPanel.get();
+    rootPanel.add(button);
+  }
+
+  @VisibleForTesting
+  interface Alerter {
+    void alert(String message);
+  }
+
 }
