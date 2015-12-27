becb960779345243020faacee064bd9b97498a1c
David Whitlock <david.m.whitlock@gmail.com>
2015-12-26 17:00:48 -0800

    Add some whitespace that is in the XML file generated by the archetype:create goal so that there are fewer differences when we regenerate the archetype source code.  Also cleaned up some import statements.

--- projects-parent/archetypes-parent/phonebill-archetype/src/main/resources/archetype-resources/src/test/java/PhoneCallTest.java
+++ projects-parent/archetypes-parent/phonebill-archetype/src/main/resources/archetype-resources/src/test/java/PhoneCallTest.java
@@ -5,9 +5,7 @@ package ${package};
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
