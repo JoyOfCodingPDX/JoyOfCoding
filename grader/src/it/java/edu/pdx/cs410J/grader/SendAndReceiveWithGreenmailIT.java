3fc0aba5722fd9add86675b1cdcb9c9304ee855e
David Whitlock <david.m.whitlock@gmail.com>
2016-03-05 11:22:32 -0800

    Refactor common code for greenmail tests into a superclass.  Add a new test for multipart messages.

--- grader/src/it/java/edu/pdx/cs410J/grader/SendAndReceiveWithGreenmailIT.java
+++ grader/src/it/java/edu/pdx/cs410J/grader/SendAndReceiveWithGreenmailIT.java
@@ -1,53 +1,15 @@
 package edu.pdx.cs410J.grader;
 
-import com.icegreen.greenmail.imap.AuthorizationException;
-import com.icegreen.greenmail.store.FolderException;
-import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.ServerSetup;
-import com.sun.mail.util.MailSSLSocketFactory;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import javax.mail.*;
-import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Properties;
 
-public class SendAndReceiveWithGreenmailIT {
-
-  private GreenMail emailServer;
-  private final String emailAddress = "test@email.com";
-  private final String emailServerHost = "127.0.0.1";
-  private final int smtpPort = 2525;
-  private final String imapUserName = "emailUser";
-  private final String imapPassword = "emailPassword";
-  private final int imapsPort = 9933;
-
-  @Before
-  public void startEmailServer() throws FolderException, AuthorizationException {
-    ServerSetup smtp = new ServerSetup(smtpPort, emailServerHost, ServerSetup.PROTOCOL_SMTP);
-    ServerSetup imaps = new ServerSetup(imapsPort, emailServerHost, ServerSetup.PROTOCOL_IMAPS);
-    emailServer = new GreenMail(new ServerSetup[]{ smtp, imaps });
-
-    emailServer.setUser(emailAddress, imapUserName, imapPassword);
-
-    emailServer.start();
-  }
-
-  @Before
-  public void enableDebugLogging() {
-    System.setProperty("mail.imap.parse.debug", Boolean.TRUE.toString());
-  }
-
-  @After
-  public void stopEmailServer() {
-    emailServer.stop();
-  }
+public class SendAndReceiveWithGreenmailIT extends GreenmailIntegrationTestCase {
 
   @Test
   public void sendAndFetchMailMessageWithInlineAttachment() throws IOException, MessagingException, GeneralSecurityException {
@@ -70,45 +32,6 @@ public class SendAndReceiveWithGreenmailIT {
     for (Message message : messages) {
       System.out.println("  Content Type: " + message.getContentType());
     }
-  }
-
-  private Folder openFolder(Store store, String folderName) throws MessagingException {
-    Folder folder = store.getDefaultFolder();
-    folder = folder.getFolder(folderName);
-    folder.open(Folder.READ_WRITE);
-    return folder;
-  }
-
-  private Store connectToIMAPServer() throws GeneralSecurityException, MessagingException {
-    Properties props = new Properties();
-
-    MailSSLSocketFactory socketFactory = new MailSSLSocketFactory();
-    socketFactory.setTrustedHosts(new String[]{"127.0.0.1", "localhost"});
-    props.put("mail.imaps.ssl.socketFactory", socketFactory);
-
-    Session session = Session.getInstance(props, null);
-    Store store = session.getStore("imaps");
-    store.connect(emailServerHost, imapsPort, imapUserName, imapPassword);
-
-    return store;
-  }
-
-  protected MimeMessage newEmailTo(Session session, String recipient, String subject) throws MessagingException {
-    MimeMessage message = new MimeMessage(session);
-
-    InternetAddress[] to = {new InternetAddress(recipient)};
-    message.setRecipients(Message.RecipientType.TO, to);
-    message.setSubject(subject);
-    return message;
-  }
-
-  protected Session newEmailSession(boolean debug) {
-    Properties props = new Properties();
-    props.put("mail.smtp.host", emailServerHost);
-    props.put("mail.smtp.port", smtpPort);
-    Session session = Session.getDefaultInstance(props, null);
-    session.setDebug(debug);
-    return session;
   }
 
   private void sendMailMessageWithInlineAttachment() throws MessagingException {
