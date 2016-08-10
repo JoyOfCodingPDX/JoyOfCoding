25142fd3fe4b74c099a9506f9f96cf44c971f281
David Whitlock <david.m.whitlock@gmail.com>
2016-08-09 21:03:39 -0700

    Copy the contents of a zip file into another.

--- grader/src/main/java/edu/pdx/cs410J/grader/GwtZipFixer.java
+++ grader/src/main/java/edu/pdx/cs410J/grader/GwtZipFixer.java
@@ -1,11 +1,15 @@
 package edu.pdx.cs410J.grader;
 
+import com.google.common.io.ByteStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.PrintStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class GwtZipFixer {
 
@@ -26,11 +30,56 @@ public class GwtZipFixer {
 
     if (outputDirectoryName == null) {
       usage("Missing output directory");
+      return;
     }
 
     if (zipFileNames.isEmpty()) {
       usage("Missing zip file");
+      return;
     }
+
+    File outputDirectory = new File(outputDirectoryName);
+
+    for (String zipFileName : zipFileNames) {
+      File zipFile = new File(zipFileName);
+      try {
+        fixZipFile(zipFile, outputDirectory);
+
+      } catch (IOException e) {
+        usage("While fixing zip file: " + e);
+      }
+
+    }
+  }
+
+  private static void fixZipFile(File zipFile, File outputDirectory) throws IOException {
+    File fixedZipFile = getFixedZipFile(zipFile, outputDirectory);
+
+    try (
+      ZipInputStream input = new ZipInputStream(new FileInputStream(zipFile));
+      ZipOutputStream output = new ZipOutputStream(new FileOutputStream(fixedZipFile));
+    ) {
+      for (ZipEntry entry = input.getNextEntry(); entry != null; entry = input.getNextEntry()) {
+        String fixedEntryName = getFixedEntryName(entry.getName());
+
+        if (fixedEntryName != null) {
+          ZipEntry fixedEntry = new ZipEntry(fixedEntryName);
+          output.putNextEntry(fixedEntry);
+
+          ByteStreams.copy(input, output);
+        }
+
+        output.flush();
+      }
+    }
+  }
+
+  private static String getFixedEntryName(String entryName) {
+    return entryName;
+  }
+
+  private static File getFixedZipFile(File zipFile, File outputDirectory) {
+    return new File(outputDirectory, zipFile.getName());
   }
 
   private static void usage(String message) {
