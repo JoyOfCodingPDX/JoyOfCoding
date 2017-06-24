28d5ce797093c84350be66aa5be46e30ac1d2f26
David Whitlock <david.m.whitlock@gmail.com>
2017-06-24 12:19:10 -0700

    Add an integration test for the PhoneBill project archetype that verifies that the JavaDoc is generated when the project is graded.

---
+++ projects-parent/archetypes-parent/phonebill-archetype/src/test/resources/projects/javadoc/verify.groovy
@@ -0,0 +1,11 @@
+File buildLog = new File(basedir, "project/javadoc/build.log");
+if (!buildLog.isFile()) {
+  throw new FileNotFoundException("Couldn't find build log: " + buildLog)
+}
+
+String logText = buildLog.text
+
+def expectedJavaDoc = "The main class for the CS410J Phone Bill Project"
+if (!logText.contains(expectedJavaDoc)) {
+  throw new IllegalStateException("Didn't find expected JavaDoc: " + expectedJavaDoc)
+}
