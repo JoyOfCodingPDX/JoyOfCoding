893a828dfe4a94d4b35427d644fa0e3a0c34366e
David Whitlock <david.m.whitlock@gmail.com>
2017-06-25 10:37:37 -0700

    Continue addressing #182 by refactoring airline web application project to not expose Responses outside of the REST client.

--- projects-parent/originals-parent/airline-web/src/main/java/edu/pdx/cs410J/airlineweb/Messages.java
+++ projects-parent/originals-parent/airline-web/src/main/java/edu/pdx/cs410J/airlineweb/Messages.java
@@ -1,12 +1,18 @@
 package edu.pdx.cs410J.airlineweb;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Class for formatting messages on the server side.  This is mainly to enable
  * test methods that validate that the server returned expected strings.
  */
 public class Messages
 {
-    public static String getMappingCount( int count )
+    public static String formatMappingCount(int count )
     {
         return String.format( "Server contains %d key/value pairs", count );
     }
@@ -28,5 +34,56 @@ public class Messages
 
     public static String allMappingsDeleted() {
         return "All mappings have been deleted";
+    }
+
+    public static Map.Entry<String, String> parseKeyValuePair(String content) {
+        Pattern pattern = Pattern.compile("\\s*(.*) -> (.*)");
+        Matcher matcher = pattern.matcher(content);
+
+        if (!matcher.find()) {
+            return null;
+        }
+
+        return new Map.Entry<String, String>() {
+            @Override
+            public String getKey() {
+                return matcher.group(1);
+            }
+
+            @Override
+            public String getValue() {
+                String value = matcher.group(2);
+                if ("null".equals(value)) {
+                    value = null;
+                }
+                return value;
+            }
+
+            @Override
+            public String setValue(String value) {
+                throw new UnsupportedOperationException("This method is not implemented yet");
+            }
+        };
+    }
+
+    public static void formatKeyValueMap(PrintWriter pw, Map<String, String> map) {
+        pw.println(Messages.formatMappingCount(map.size()));
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            pw.println(Messages.formatKeyValuePair(entry.getKey(), entry.getValue()));
+        }
+    }
+
+    public static Map<String, String> parseKeyValueMap(String content) {
+        Map<String, String> map = new HashMap<>();
+
+        String[] lines = content.split("\n");
+        for (int i = 1; i < lines.length; i++) {
+            String line = lines[i];
+            Map.Entry<String, String> entry = parseKeyValuePair(line);
+            map.put(entry.getKey(), entry.getValue());
+        }
+
+        return map;
     }
 }
