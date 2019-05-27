cbd4c1dbad087dcf346ed8ba507f9593da3cbe1c
David Whitlock <david.m.whitlock@gmail.com>
2019-05-27 09:24:15 -0700

    wagon-git commit

--- examples/testapidocs/package-search-index.js
+++ examples/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"edu.pdx.cs410J.di"},{"l":"edu.pdx.cs410J.junit"},{"l":"edu.pdx.cs410J.rmi"},{"l":"edu.pdx.cs410J.web"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.pdx.cs410J.di"},{"l":"edu.pdx.cs410J.junit"},{"l":"edu.pdx.cs410J.rmi"},{"l":"edu.pdx.cs410J.web"}]
