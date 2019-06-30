c63184b1af4f6d1352ea5efcc8aaa3ea69b83607
David Whitlock <david.m.whitlock@gmail.com>
2019-06-30 08:02:36 -0700

    Add a new test so that we do not have an empty set of unit tests.  That situation now causes the build to fail.

---
+++ projects-parent/projects/src/test/java/edu/pdx/cs410J/InvokeMainTest.java
@@ -0,0 +1,30 @@
+package edu.pdx.cs410J;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+public class InvokeMainTest extends InvokeMainTestCase {
+
+  @Test
+  public void commandLineArgsWrittenToStandardOutAreCaptured() {
+    String[] args = { "One", "Two", "Three"};
+    MainMethodResult result = invokeMain(InvokeMainTest.class, args);
+    assertThat(result.getExitCode(), equalTo(0));
+
+    String out = result.getTextWrittenToStandardOut();
+    for(String arg : args) {
+      assertThat(out, containsString(arg));
+    }
+  }
+
+  public static void main(String... args) {
+    for (String arg : args) {
+      System.out.println(arg);
+    }
+
+    System.exit(0);
+  }
+}
