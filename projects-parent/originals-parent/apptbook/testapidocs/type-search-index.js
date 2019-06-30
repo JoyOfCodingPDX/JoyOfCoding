8d1cce29ca1c885810e90f07934516e3ffb5f205
David Whitlock <david.m.whitlock@gmail.com>
2019-06-29 22:48:44 -0700

    wagon-git commit

--- projects-parent/originals-parent/apptbook/testapidocs/type-search-index.js
+++ projects-parent/originals-parent/apptbook/testapidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.apptbook","l":"AppointmentTest"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.apptbook","l":"AppointmentTest"},{"p":"edu.pdx.cs410J.apptbook","l":"Project1IT"}]
