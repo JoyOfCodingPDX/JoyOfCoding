b017dc3ba478236be6e6e2a3d2882961a076f3fb
David Whitlock <david.m.whitlock@gmail.com>
2019-07-05 09:36:34 -0700

    wagon-git commit

--- projects-parent/originals-parent/student/testapidocs/type-search-index.js
+++ projects-parent/originals-parent/student/testapidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.student","l":"StudentIT"},{"p":"edu.pdx.cs410J.student","l":"StudentTest"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.student","l":"StudentTest"}]
