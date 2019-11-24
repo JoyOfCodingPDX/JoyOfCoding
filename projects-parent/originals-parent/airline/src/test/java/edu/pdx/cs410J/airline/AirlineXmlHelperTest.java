7354d08f414e34e9a6a4e71abc6d31d459c627ab
David Whitlock <david.m.whitlock@gmail.com>
2019-11-23 20:39:57 -0800

    Add a helper class for parsing XML files that conform to the project DTDs.

---
+++ projects-parent/originals-parent/airline/src/test/java/edu/pdx/cs410J/airline/AirlineXmlHelperTest.java
@@ -0,0 +1,48 @@
+package edu.pdx.cs410J.airline;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class AirlineXmlHelperTest {
+
+  @Test
+  public void canParseValidXmlFile() throws ParserConfigurationException, IOException, SAXException {
+    AirlineXmlHelper helper = new AirlineXmlHelper();
+
+
+    DocumentBuilderFactory factory =
+      DocumentBuilderFactory.newInstance();
+    factory.setValidating(true);
+
+    DocumentBuilder builder =
+      factory.newDocumentBuilder();
+    builder.setErrorHandler(helper);
+    builder.setEntityResolver(helper);
+
+    builder.parse(this.getClass().getResourceAsStream("valid-airline.xml"));
+  }
+
+  @Test(expected = SAXParseException.class)
+  public void cantParseInvalidXmlFile() throws ParserConfigurationException, IOException, SAXException {
+    AirlineXmlHelper helper = new AirlineXmlHelper();
+
+
+    DocumentBuilderFactory factory =
+      DocumentBuilderFactory.newInstance();
+    factory.setValidating(true);
+
+    DocumentBuilder builder =
+      factory.newDocumentBuilder();
+    builder.setErrorHandler(helper);
+    builder.setEntityResolver(helper);
+
+    builder.parse(this.getClass().getResourceAsStream("invalid-airline.xml"));
+  }
+
+}
