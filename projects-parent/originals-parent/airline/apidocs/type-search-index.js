da0e2f518660b3b7b57536ff75ef631e1f0ec299
David Whitlock <david.m.whitlock@gmail.com>
2020-01-05 06:46:41 -0800

    wagon-git commit

--- projects-parent/originals-parent/airline/apidocs/type-search-index.js
+++ projects-parent/originals-parent/airline/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.airline","l":"Flight"},{"p":"edu.pdx.cs410J.airline","l":"Project1"}]
+typeSearchIndex = [{"p":"edu.pdx.cs410J.airline","l":"AirlineXmlHelper"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.airline","l":"Flight"},{"p":"edu.pdx.cs410J.airline","l":"Project1"}]
--- projects-parent/originals-parent/airline/testapidocs/type-search-index.js
+++ projects-parent/originals-parent/airline/testapidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.airline","l":"FlightTest"}]
+typeSearchIndex = [{"p":"edu.pdx.cs410J.airline","l":"AirlineXmlHelperTest"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.airline","l":"FlightTest"}]
