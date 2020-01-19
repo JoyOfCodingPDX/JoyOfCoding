dfd2209a2d8cd42f09afc9ac6a88707bb432e7dd
David Whitlock <david.m.whitlock@gmail.com>
2020-01-18 21:59:40 -0800

    Looks like these changes from (https://github.com/DavidWhitlock/PortlandStateJava/commit/60b794fc0d730fab75bcee608373d9d5cd55ac2f) address #281 for the airline web project.

--- projects-parent/archetypes-parent/airline-web-archetype/src/test/resources/projects/basic/verify.groovy
+++ projects-parent/archetypes-parent/airline-web-archetype/src/test/resources/projects/basic/verify.groovy
@@ -1,4 +1,13 @@
 def projectDir = new File(basedir, "project/basic")
+File mvnwFile = new File(projectDir, "mvnw");
+if (!mvnwFile.isFile()) {
+  throw new FileNotFoundException("Couldn't find mvn wrapper: " + mvnwFile)
+}
+
+File mvnwCmdFile = new File(projectDir, "mvnw.cmd");
+if (!mvnwCmdFile.isFile()) {
+  throw new FileNotFoundException("Couldn't find mvn wrapper: " + mvnwCmdFile)
+}
 
 File targetDir = new File(projectDir, "target");
 
