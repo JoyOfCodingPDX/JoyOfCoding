0cd14888d280bbed142825f86e03dbaff15c66e1
David Whitlock <david.m.whitlock@gmail.com>
2020-01-26 08:25:28 -0800

    Renamed the project to "Kata" and got rid of references to "Student".

--- projects-parent/originals-parent/student/src/it/java/edu/pdx/cs410J/student/KataIT.java
+++ projects-parent/originals-parent/student/src/it/java/edu/pdx/cs410J/student/KataIT.java
@@ -8,20 +8,20 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
 
 /**
- * Integration tests for the <code>Student</code> class's main method.
+ * Integration tests for the <code>Kata</code> class's main method.
  * These tests extend <code>InvokeMainTestCase</code> which allows them
- * to easily invoke the <code>main</code> method of <code>Student</code>.
+ * to easily invoke the <code>main</code> method of <code>Kata</code>.
  */
-public class StudentIT extends InvokeMainTestCase {
+public class KataIT extends InvokeMainTestCase {
   @Test
   public void invokingMainWithNoArgumentsHasExitCodeOf1() {
-    InvokeMainTestCase.MainMethodResult result = invokeMain(Student.class);
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
     assertThat(result.getExitCode(), equalTo(1));
   }
 
   @Test
   public void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
-    InvokeMainTestCase.MainMethodResult result = invokeMain(Student.class);
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
   }
 
