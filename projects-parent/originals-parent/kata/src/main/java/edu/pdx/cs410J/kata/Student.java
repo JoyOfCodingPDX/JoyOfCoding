2b7b680f6ebeba90734eb2bb4afff93329e74f6d
David Whitlock <david.m.whitlock@gmail.com>
2020-01-26 08:20:41 -0800

    Added a new "original" project for code katas

---
+++ projects-parent/originals-parent/kata/src/main/java/edu/pdx/cs410J/kata/Student.java
@@ -0,0 +1,54 @@
+package edu.pdx.cs410J.kata;
+
+import edu.pdx.cs410J.lang.Human;
+
+import java.util.ArrayList;
+                                                                                    
+/**                                                                                 
+ * This class is represents a <code>Student</code>.                                 
+ */                                                                                 
+public class Student extends Human {                                                
+                                                                                    
+  /**                                                                               
+   * Creates a new <code>Student</code>                                             
+   *                                                                                
+   * @param name                                                                    
+   *        The kata's name                                                      
+   * @param classes                                                                 
+   *        The names of the classes the kata is taking.  A kata              
+   *        may take zero or more classes.                                          
+   * @param gpa                                                                     
+   *        The kata's grade point average                                       
+   * @param gender                                                                  
+   *        The kata's gender ("male" or "female", or "other", case insensitive)
+   */                                                                               
+  public Student(String name, ArrayList<String> classes, double gpa, String gender) {
+    super(name);
+  }
+
+  /**                                                                               
+   * All students say "This class is too much work"
+   */
+  @Override
+  public String says() {                                                            
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+                                                                                    
+  /**                                                                               
+   * Returns a <code>String</code> that describes this                              
+   * <code>Student</code>.                                                          
+   */                                                                               
+  public String toString() {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  /**
+   * Main program that parses the command line, creates a
+   * <code>Student</code>, and prints a description of the kata to
+   * standard out by invoking its <code>toString</code> method.
+   */
+  public static void main(String[] args) {
+    System.err.println("Missing command line arguments");
+    System.exit(1);
+  }
+}
---
+++ projects-parent/originals-parent/kata/src/test/java/edu/pdx/cs410J/kata/StudentTest.java
@@ -0,0 +1,25 @@
+package edu.pdx.cs410J.kata;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Unit tests for the Student class.  In addition to the JUnit annotations,
+ * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
+ * matchers for more readable assertion statements.
+ */
+public class StudentTest
+{
+
+  @Test
+  public void studentNamedPatIsNamedPat() {
+    String name = "Pat";
+    var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
+    assertThat(pat.getName(), equalTo(name));
+  }
+
+}
