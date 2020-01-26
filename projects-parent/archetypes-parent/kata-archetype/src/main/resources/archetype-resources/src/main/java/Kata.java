a276cd2d716b3d5fc768b356cf8f0bacf53abab0
David Whitlock <david.m.whitlock@gmail.com>
2020-01-26 08:38:33 -0800

    Create an archetype project for the Kata project.

---
+++ projects-parent/archetypes-parent/kata-archetype/src/main/resources/archetype-resources/src/main/java/Kata.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+public class Kata {
+                                                                                    
+
+  public static void main(String[] args) {
+    System.err.println("Missing command line arguments");
+    System.exit(1);
+  }
+}
