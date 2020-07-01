1bc7b7643e8f4ae3ec8eb188a2938fac0f658401
David Whitlock <david.m.whitlock@gmail.com>
2020-06-30 21:19:03 -0700

    Move the version configuration for the maven-source-plugin and the maven-jar-plugin into the <pluginManagement> section of the parent POM so that it can be shared among all sub-modules.
    Newer versions of the maven-jar-plugin require that the jar file created as part of the web projects have a "classifier".

--- projects-parent/archetypes-parent/airline-web-archetype/src/test/resources/projects/basic/verify.groovy
+++ projects-parent/archetypes-parent/airline-web-archetype/src/test/resources/projects/basic/verify.groovy
@@ -11,7 +11,7 @@ if (!mvnwCmdFile.isFile()) {
 
 File targetDir = new File(projectDir, "target");
 
-File jarFile = new File(targetDir, "airline.jar");
+File jarFile = new File(targetDir, "airline-client.jar");
 if (!jarFile.isFile()) {
   throw new FileNotFoundException("Couldn't find jar file: " + jarFile)
 }
