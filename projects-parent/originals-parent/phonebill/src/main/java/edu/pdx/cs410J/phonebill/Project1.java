9c1a0a7ce618ecf1f6c4082c98e3189508844724
David Whitlock <david.m.whitlock@gmail.com>
2020-07-11 10:41:02 -0700

    Begin to address #262 by adding the guava dependency to the phone bill project.

--- projects-parent/originals-parent/phonebill/src/main/java/edu/pdx/cs410J/phonebill/Project1.java
+++ projects-parent/originals-parent/phonebill/src/main/java/edu/pdx/cs410J/phonebill/Project1.java
@@ -1,9 +1,16 @@
 package edu.pdx.cs410J.phonebill;
 
+import com.google.common.annotations.VisibleForTesting;
+
 /**
  * The main class for the CS410J Phone Bill Project
  */
 public class Project1 {
+
+  @VisibleForTesting
+  static boolean isValidPhoneNumber(String phoneNumber) {
+    return true;
+  }
 
   public static void main(String[] args) {
     PhoneCall call = new PhoneCall();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
