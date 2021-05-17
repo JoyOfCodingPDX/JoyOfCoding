ee9ee12141e9d0cac1860d04f51682cef1fc14d8
David Whitlock <david.m.whitlock@gmail.com>
2021-05-16 20:11:22 -0700

    wagon-git commit

--- family/testapidocs/package-search-index.js
+++ family/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.pdx.cs410J.family"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.pdx.cs410J.family"}];updateSearchResults();
