923f3197db85a539ffd56cd9ed417add18f0c90d
David Whitlock <david.m.whitlock@gmail.com>
2021-05-16 20:48:45 -0700

    wagon-git commit

--- projects-parent/originals-parent/airline-web/testapidocs/package-search-index.js
+++ projects-parent/originals-parent/airline-web/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.pdx.cs410J.airlineweb"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.pdx.cs410J.airlineweb"}];updateSearchResults();
