54cf3e005361b45e1cc073a14faf1c1668fd17e5
David Whitlock <david.m.whitlock@gmail.com>
2021-05-16 20:44:25 -0700

    wagon-git commit

--- projects-parent/originals-parent/student/apidocs/type-search-index.js
+++ projects-parent/originals-parent/student/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.pdx.cs410J.student","l":"Student"}]
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"edu.pdx.cs410J.student","l":"Student"}];updateSearchResults();
--- projects-parent/originals-parent/student/testapidocs/member-search-index.js
+++ projects-parent/originals-parent/student/testapidocs/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"edu.pdx.cs410J.student","c":"StudentTest","l":"studentNamedPatIsNamedPat()"},{"p":"edu.pdx.cs410J.student","c":"StudentTest","l":"StudentTest()","url":"%3Cinit%3E()"}]
+memberSearchIndex = [{"p":"edu.pdx.cs410J.student","c":"StudentTest","l":"studentNamedPatIsNamedPat()"},{"p":"edu.pdx.cs410J.student","c":"StudentTest","l":"StudentTest()","u":"%3Cinit%3E()"}];updateSearchResults();
