8be06517368378b18477b2a39d5f577189c04363
David Whitlock <david.m.whitlock@gmail.com>
2021-06-24 06:10:38 -0700

    Finally address #136 by augmenting the "basic" integration test for the archetype projects to run the executable jar file.

--- projects-parent/archetypes-parent/apptbook-archetype/src/test/resources/projects/basic/verify.groovy
+++ projects-parent/archetypes-parent/apptbook-archetype/src/test/resources/projects/basic/verify.groovy
@@ -9,3 +9,10 @@ if (!mvnwCmdFile.isFile()) {
   throw new FileNotFoundException("Couldn't find mvn wrapper: " + mvnwCmdFile)
 }
 
+String jarCommand = "java -jar ${projectDir}/target/basic-0.1-SNAPSHOT.jar"
+def execution = jarCommand.execute()
+execution.waitFor()
+String stderr = execution.err.text
+if (!stderr.contains("Missing command line arguments")) {
+  throw new IllegalStateException("Running jar returned \"" + stderr + "\"");
+}
