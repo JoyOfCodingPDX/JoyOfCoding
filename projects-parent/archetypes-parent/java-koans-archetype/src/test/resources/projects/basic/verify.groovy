d94a44d8d5e85c68180f82e7f434f082106b8741
David Whitlock <david.m.whitlock@gmail.com>
2021-07-01 22:10:14 -0700

    Continue to address https://github.com/DavidWhitlock/java-koans/issues/35 by removing the README.md from the Java Koans archetype.

--- projects-parent/archetypes-parent/java-koans-archetype/src/test/resources/projects/basic/verify.groovy
+++ projects-parent/archetypes-parent/java-koans-archetype/src/test/resources/projects/basic/verify.groovy
@@ -6,8 +6,6 @@ import java.nio.file.Paths
 import java.util.function.Function
 import java.util.function.Predicate
 import java.util.stream.Collectors
-import java.util.stream.Stream
-import java.util.stream.StreamSupport
 
 def projectDir = new File(basedir, "project/basic")
 File mvnwFile = new File(projectDir, "mvnw");
@@ -20,22 +18,28 @@ if (!mvnwCmdFile.isFile()) {
   throw new FileNotFoundException("Couldn't find mvn wrapper: " + mvnwCmdFile)
 }
 
-def walk = Files.walk(Paths.get(projectDir.toURI()))
-List<Path> gradleFiles = walk.filter(new Predicate<Path>() {
-  @Override
-  boolean test(Path path) {
-    return path.toString().contains("gradle");
-  }
-}).map(new Function<Path, String>() {
-  @Override
-  String apply(Path path) {
-    path.toString();
-  }
-}).collect(Collectors.toList());
+validateNoFilesInDirectoryWithNameContaining(projectDir, "gradle")
+validateNoFilesInDirectoryWithNameContaining(projectDir, "README.md")
+
+private static void validateNoFilesInDirectoryWithNameContaining(File projectDir, String query) {
+  def walk = Files.walk(Paths.get(projectDir.toURI()))
+  List<Path> gradleFiles = walk.filter(new Predicate<Path>() {
+    @Override
+    boolean test(Path path) {
+      return path.toString().contains(query);
+    }
+  }).map(new Function<Path, String>() {
+    @Override
+    String apply(Path path) {
+      path.toString();
+    }
+  }).collect(Collectors.toList());
 
 
-if (!gradleFiles.isEmpty()) {
-  String s = "Found " + gradleFiles.size() + " gradle files: " +
-    gradleFiles.stream().collect(Collectors.joining(", "));
-  throw new IllegalStateException(s);
+  if (!gradleFiles.isEmpty()) {
+    String s = "Found " + gradleFiles.size() + " " + query + " files: " +
+            gradleFiles.stream().collect(Collectors.joining(", "));
+    throw new IllegalStateException(s);
+  }
 }
+
