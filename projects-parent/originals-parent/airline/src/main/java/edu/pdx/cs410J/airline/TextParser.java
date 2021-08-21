015152ba841bbc925dfa65a90097d18dcd5df509
David Whitlock <david.m.whitlock@gmail.com>
2021-08-21 11:03:48 -0700

    Begin addressing https://github.com/DavidWhitlock/PortlandStateJava/issues/311 by adding TextParser and TextDumper examples and tests.

---
+++ projects-parent/originals-parent/airline/src/main/java/edu/pdx/cs410J/airline/TextParser.java
@@ -0,0 +1,36 @@
+package edu.pdx.cs410J.airline;
+
+import edu.pdx.cs410J.AirlineParser;
+import edu.pdx.cs410J.ParserException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class TextParser implements AirlineParser<Airline> {
+  private final Reader reader;
+
+  public TextParser(Reader reader) {
+    this.reader = reader;
+  }
+
+  @Override
+  public Airline parse() throws ParserException {
+    try (
+      BufferedReader br = new BufferedReader(this.reader)
+    ) {
+
+      String airlineName = br.readLine();
+
+      if (airlineName == null) {
+        throw new ParserException("Missing airline name");
+      }
+
+      return new Airline(airlineName);
+
+    } catch (IOException e) {
+      throw new ParserException("While parsing airline text", e);
+    }
+  }
+}
