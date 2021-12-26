a14d6ffaa541c815719329c57f0bdf7e90588c0c
David Whitlock <david.m.whitlock@gmail.com>
2021-12-25 19:12:25 -0800

    wagon-git commit

--- web/testapidocs/type-search-index.js
+++ web/testapidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"edu.pdx.cs410J.di","l":"CreditCardDatabaseTest"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.pdx.cs410J.di","l":"CreditCardDatabaseTest"}];updateSearchResults();
