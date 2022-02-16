99d0ad5e13ac2839a3430971005283527fc35cee
David Whitlock <david.m.whitlock@gmail.com>
2022-02-15 19:34:51 -0800

    Fully fix #262 by adding the guava dependency to all original projects.

--- projects-parent/originals-parent/apptbook/src/main/java/edu/pdx/cs410J/apptbook/Project1.java
+++ projects-parent/originals-parent/apptbook/src/main/java/edu/pdx/cs410J/apptbook/Project1.java
@@ -1,9 +1,16 @@
 package edu.pdx.cs410J.apptbook;
 
+import com.google.common.annotations.VisibleForTesting;
+
 /**
  * The main class for the CS410J appointment book Project
  */
 public class Project1 {
+
+  @VisibleForTesting
+  static boolean isValidDateAndTime(String dateAndTime) {
+    return true;
+  }
 
   public static void main(String[] args) {
     Appointment appointment = new Appointment();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
