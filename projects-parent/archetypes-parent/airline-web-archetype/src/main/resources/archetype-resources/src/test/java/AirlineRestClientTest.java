5bdd2276b7e024f9945766bf8e0039d21bc3e28b
David Whitlock <david.m.whitlock@gmail.com>
2022-05-14 07:16:49 -0700

    Updated airline web archetype with the new injectable HTTPRequestHelper.

---
+++ projects-parent/archetypes-parent/airline-web-archetype/src/main/resources/archetype-resources/src/test/java/AirlineRestClientTest.java
@@ -0,0 +1,43 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import edu.pdx.cs410J.ParserException;
+import edu.pdx.cs410J.web.NewHttpRequestHelper;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+/**
+ * A unit test for the REST client that demonstrates using mocks and
+ * dependency injection
+ */
+public class AirlineRestClientTest {
+
+  @Test
+  void getAllDictionaryEntriesPerformsHttpGetWithNoParameters() throws ParserException, IOException {
+    Map<String, String> dictionary = Map.of("One", "1", "Two", "2");
+
+    NewHttpRequestHelper http = mock(NewHttpRequestHelper.class);
+    when(http.get(eq(Map.of()))).thenReturn(dictionaryAsText(dictionary));
+    
+    AirlineRestClient client = new AirlineRestClient(http);
+
+    assertThat(client.getAllDictionaryEntries(), equalTo(dictionary));
+  }
+
+  private NewHttpRequestHelper.Response dictionaryAsText(Map<String, String> dictionary) {
+    StringWriter writer = new StringWriter();
+    new TextDumper(writer).dump(dictionary);
+
+    return new NewHttpRequestHelper.Response(writer.toString());
+  }
+}
