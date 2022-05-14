33c348c8d149d7b2a25177b89a7619149301ec94
David Whitlock <david.m.whitlock@gmail.com>
2022-05-14 07:33:21 -0700

    Use the NewHttpRequestHelper in the AppointmentBookRestClient.

--- projects-parent/originals-parent/apptbook-web/src/it/java/edu/pdx/cs410J/apptbookweb/Project4IT.java
+++ projects-parent/originals-parent/apptbook-web/src/it/java/edu/pdx/cs410J/apptbookweb/Project4IT.java
@@ -2,7 +2,7 @@ package edu.pdx.cs410J.apptbookweb;
 
 import edu.pdx.cs410J.InvokeMainTestCase;
 import edu.pdx.cs410J.UncaughtExceptionInMain;
-import edu.pdx.cs410J.web.HttpRequestHelper.RestException;
+import edu.pdx.cs410J.web.NewHttpRequestHelper.RestException;
 import org.junit.jupiter.api.MethodOrderer.MethodName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
