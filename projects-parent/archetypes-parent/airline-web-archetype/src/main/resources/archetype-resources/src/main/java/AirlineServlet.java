c6a939041b14af8037c3b901b7837aa44d012a1b
David Whitlock <david.m.whitlock@gmail.com>
2024-09-14 21:42:33 -0700

    Move the servlet logging to the archetypes.

--- projects-parent/archetypes-parent/airline-web-archetype/src/main/resources/archetype-resources/src/main/java/AirlineServlet.java
+++ projects-parent/archetypes-parent/airline-web-archetype/src/main/resources/archetype-resources/src/main/java/AirlineServlet.java
@@ -38,9 +38,11 @@ public class AirlineServlet extends HttpServlet {
 
       String word = getParameter( WORD_PARAMETER, request );
       if (word != null) {
+          log("GET " + word);
           writeDefinition(word, response);
 
       } else {
+          log("GET all dictionary entries");
           writeAllDictionaryEntries(response);
       }
   }
@@ -67,6 +69,8 @@ public class AirlineServlet extends HttpServlet {
           return;
       }
 
+      log("POST " + word + " -> " + definition);
+
       this.dictionary.put(word, definition);
 
       PrintWriter pw = response.getWriter();
@@ -84,6 +88,8 @@ public class AirlineServlet extends HttpServlet {
   @Override
   protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
       response.setContentType("text/plain");
+
+      log("DELETE all dictionary entries");
 
       this.dictionary.clear();
 
