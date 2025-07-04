4fc036f7df9f472057fa0540bd94582a189dfb1f
David Whitlock <david.m.whitlock@gmail.com>
2025-07-04 16:17:58 -0700

    Refactor again to compare using a Comparable.

--- grader/src/main/java/edu/pdx/cs/joy/grader/GenerateStudentInitialsFile.java
+++ grader/src/main/java/edu/pdx/cs/joy/grader/GenerateStudentInitialsFile.java
@@ -4,8 +4,7 @@ import edu.pdx.cs.joy.grader.gradebook.Student;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toCollection;
@@ -13,24 +12,14 @@ import static java.util.stream.Collectors.toCollection;
 public class GenerateStudentInitialsFile {
 
   public static List<String> generateInitials(Stream<Student> students) {
-    List<StudentInitials> studentInitials = students
-      .sorted(GenerateStudentInitialsFile::sortStudentsByLastThenFirstName)
+    return students
       .map(StudentInitials::new)
-      .collect(toCollection(ArrayList::new));
-    return studentInitials.stream()
+      .sorted()
       .map(StudentInitials::getInitials)
       .collect(toCollection(ArrayList::new));
   }
 
-  private static int sortStudentsByLastThenFirstName(Student s1, Student s2) {
-    int lastNameComparison = s1.getLastName().compareToIgnoreCase(s2.getLastName());
-    if (lastNameComparison != 0) {
-      return lastNameComparison;
-    }
-    return s1.getFirstName().compareToIgnoreCase(s2.getFirstName());
-  }
-
-  private static class StudentInitials {
+  private static class StudentInitials implements Comparable<StudentInitials> {
     private final Student student;
     int uniqueCharsOfFirstname = 1;
     int uniqueCharsOfLastname = 1;
@@ -40,8 +29,28 @@ public class GenerateStudentInitialsFile {
     }
 
     public String getInitials() {
-      return student.getFirstName().substring(0, uniqueCharsOfFirstname).toUpperCase()
-        + student.getLastName().substring(0, uniqueCharsOfLastname).toUpperCase();
+      return getFirstNameInitials() + getLastNameInitials();
+    }
+
+    private String getLastNameInitials() {
+      return getNameInitials(student::getLastName, uniqueCharsOfLastname);
+    }
+
+    private static String getNameInitials(Supplier<String> nameAccessor, int uniqueChars) {
+      return nameAccessor.get().substring(0, uniqueChars).toUpperCase();
+    }
+
+    private String getFirstNameInitials() {
+      return getNameInitials(student::getFirstName, uniqueCharsOfFirstname);
+    }
+
+    @Override
+    public int compareTo(StudentInitials other) {
+      int lastInitialsComparison = this.getLastNameInitials().compareToIgnoreCase(other.getLastNameInitials());
+      if (lastInitialsComparison != 0) {
+        return lastInitialsComparison;
+      }
+      return this.getFirstNameInitials().compareToIgnoreCase(other.getFirstNameInitials());
     }
   }
 }
