80af9f9efc9b38f8037a4dc99bb7d02c9d835746
David Whitlock <david.m.whitlock@gmail.com>
2025-07-04 12:00:27 -0700

    Generate initials for a bunch of students.

--- grader/src/main/java/edu/pdx/cs/joy/grader/GenerateStudentInitialsFile.java
+++ grader/src/main/java/edu/pdx/cs/joy/grader/GenerateStudentInitialsFile.java
@@ -2,6 +2,14 @@ package edu.pdx.cs.joy.grader;
 
 import edu.pdx.cs.joy.grader.gradebook.Student;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toCollection;
+
 public class GenerateStudentInitialsFile {
   public static String computeInitials(Student student) {
     return String.valueOf(getInitial(student.getFirstName())) +
@@ -10,5 +18,20 @@ public class GenerateStudentInitialsFile {
 
   private static char getInitial(String word) {
     return Character.toUpperCase(word.charAt(0));
+  }
+
+  public static List<String> generateInitials(Stream<Student> students) {
+    Stream<Student> studentsByName = students
+      .sorted(GenerateStudentInitialsFile::sortStudentsByLastThenFirstName);
+    return studentsByName.map(GenerateStudentInitialsFile::computeInitials)
+      .collect(toCollection(ArrayList::new));
+  }
+
+  private static int sortStudentsByLastThenFirstName(Student s1, Student s2) {
+    int lastNameComparison = s1.getLastName().compareToIgnoreCase(s2.getLastName());
+    if (lastNameComparison != 0) {
+      return lastNameComparison;
+    }
+    return s1.getFirstName().compareToIgnoreCase(s2.getFirstName());
   }
 }
--- grader/src/test/java/edu/pdx/cs/joy/grader/GenerateStudentInitialsFileTest.java
+++ grader/src/test/java/edu/pdx/cs/joy/grader/GenerateStudentInitialsFileTest.java
@@ -3,18 +3,31 @@ package edu.pdx.cs.joy.grader;
 import edu.pdx.cs.joy.grader.gradebook.Student;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class GenerateStudentInitialsFileTest {
 
   @Test
-  void computeInitialsForAStudent() {
-    Student student = new Student("dave")
-      .setFirstName("David")
-      .setLastName("Whitlock");
+  void generatedInitialsAreSortedByLastNameInitial() {
+    Stream<Student> students = Stream.of(new Student("dave").setFirstName("Zoe").setLastName("Adams"),
+      new Student("alice").setFirstName("Alice").setLastName("Smith"),
+      new Student("bob").setFirstName("Bob").setLastName("Johnson"));
 
-    assertThat(GenerateStudentInitialsFile.computeInitials(student), equalTo("DW"));
+    List<String> initials = GenerateStudentInitialsFile.generateInitials(students);
+
+    assertThat(initials, hasSize(3));
+
+    Iterator<String> iterator = initials.iterator();
+    assertThat(iterator.next(), equalTo("ZA"));
+    assertThat(iterator.next(), equalTo("BJ"));
+    assertThat(iterator.next(), equalTo("AS"));
   }
 
 }
