030e476c1e89ccfacab678eb2ecaffac7f837cd4
David Whitlock <david.m.whitlock@gmail.com>
2025-11-28 05:18:58 -0800

    Include XML parsing example in the Phone Bill archetype.

---
+++ projects-parent/archetypes-parent/phonebill-archetype/src/main/resources/archetype-resources/src/main/java/PhoneBillXmlHelper.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import edu.pdx.cs.joy.ProjectXmlHelper;
+
+public class PhoneBillXmlHelper extends ProjectXmlHelper {
+
+  /**
+   * The Public ID for the Family Tree DTD
+   */
+  protected static final String PUBLIC_ID =
+    "-//Joy of Coding at PSU//DTD Phone Bill//EN";
+
+  protected PhoneBillXmlHelper() {
+    super(PUBLIC_ID, "phonebill.dtd");
+  }
+}
